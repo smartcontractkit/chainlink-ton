bc586af4b1c05e6f8e803b44fd57e50883d84b86
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-04-23 14:54:27 +0200

    Add nix #contracts package build (compiled artifacts)

--- contracts/default.nix
+++ contracts/default.nix
@@ -7,6 +7,8 @@
     inherit pkgs;
     inherit rev;
   };
+
+  package-info = builtins.fromJSON (builtins.readFile ./package.json);
 in {
   description = "Chainlink TON - smart contracts module";
 
@@ -18,6 +20,31 @@ in {
   # Output a set of specifc packages
   packages = {
     # Chainlink contract pkgs
-    contracts = "TODO - contracts derivation (pkg which outputs artifacts)";
+    contracts = pkgs.stdenv.mkDerivation (finalAttrs: {
+      inherit (package-info) version;
+      pname = "chainlink-contracts-ton";
+      src = ./.;
+
+      yarnOfflineCache = pkgs.fetchYarnDeps {
+        yarnLock = finalAttrs.src + "/yarn.lock";
+        # pin the vendor hash (update using 'pkgs.lib.fakeHash')
+        hash = "sha256-jpmr/Dke32UJmdCLYlpJAc4rxkARd2n5vDG52D3pVVw=";
+      };
+
+      nativeBuildInputs = with pkgs; [
+        yarnConfigHook
+        yarnBuildHook
+        yarnInstallHook
+        # Needed for executing package.json scripts
+        nodejs_23
+      ];
+
+      meta = with pkgs.lib; {
+        inherit (package-info) description;
+        license = licenses.mit;
+        # TODO: update to contracts project-specific tag
+        changelog = "https://github.com/smartcontractkit/chainlink-ton/releases/tag/v${version}";
+      };
+    });
   };
 }
