af3fc7c4d6cd509de218018fccc805bd6a9872d6
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-04-24 13:52:07 +0200

    cOunter typo

--- contracts/scripts/examples/deployCounter.ts
+++ contracts/scripts/examples/deployCounter.ts
@@ -2,8 +2,8 @@ import { Counter } from '../../wrappers/examples/Counter';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const cOunter = provider.open(await Counter.fromInit(1337n, 13n));
-    await provider.waitForDeploy(cOunter.address);
+    const counter = provider.open(await Counter.fromInit(1337n, 13n));
+    await provider.waitForDeploy(counter.address);
 
     // run methods on `Counter`
 }
