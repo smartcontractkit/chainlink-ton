37d2e2d7ac1fb21037cac51e371044bc155edf85
vicente <vicentevieytes@hotmail.com>
2025-05-07 10:55:48 -0300

    rename example contract

--- contracts/wrappers/OwnableCounter.compile.ts
+++ contracts/wrappers/OwnableCounter.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/examples/ownable_2step.tact',
+    target: 'contracts/examples/ownable_2step_counter.tact',
     options: {
         debug: true,
     },
