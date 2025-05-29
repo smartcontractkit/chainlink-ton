9a7968ffde0741e7d47aa97b8d045700b071cc3a
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2025-05-29 19:24:08 -0300

    fix: testing script

--- pkg/tonutils/tests/test.sh
+++ pkg/tonutils/tests/test.sh
@@ -5,6 +5,6 @@ nix develop .#contracts -c go test -v -parallel 1 -timeout 30m
 # Wait for docker container to stop
 sleep 30
 
-async_communication
+pushd async_communication
 nix develop .#contracts -c go test -v -parallel 1 -timeout 30m
 popd
