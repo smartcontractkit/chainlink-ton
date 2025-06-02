d60fb0e03241029ceef45e4f007dc6482b8c305d
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-06-02 20:09:51 +0200

    Update nix lock for contracts

--- contracts/default.nix
+++ contracts/default.nix
@@ -23,7 +23,7 @@ in {
       yarnOfflineCache = pkgs.fetchYarnDeps {
         inherit yarnLock;
         # pin the vendor hash (update using 'pkgs.lib.fakeHash')
-        hash = "sha256-BMqwnduetJxN2hdj+7CsFv6hrDMKpoHeO+RK66RuKrY=";
+        hash = "sha256-DYPn4ETtsjFaznZaA8R5ARwrP89J3UE+3wVtxtuw7p0=";
       };
 
       # postPatch script to copy root yarn.lock to the current build directory (and make it writeable)
