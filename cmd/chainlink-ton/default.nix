71fc679bb894b4879a86b4254859eeb3900e7a41
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2025-06-04 10:28:12 -0300

    fix: nix hash

--- cmd/chainlink-ton/default.nix
+++ cmd/chainlink-ton/default.nix
@@ -18,7 +18,7 @@ in
     ];
 
     # pin the vendor hash (update using 'pkgs.lib.fakeHash')
-    vendorHash = "sha256-/Dy1+OA6fu2LiZRC62S+M85Sfh5juubSBHFBSqOIyT4=";
+    vendorHash = "sha256-oVh/gU18zMUzI5kjwGV0cJOXzixAmEi2boRnOZD7wzw=";
 
     # postInstall script to write version and rev to share folder
     postInstall = ''
