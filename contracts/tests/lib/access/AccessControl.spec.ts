0d43fb0636c33414ef40ea75ee65b989781a897d
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-06-17 20:15:33 +0200

    Work on TS bindings

--- contracts/tests/lib/access/AccessControl.spec.ts
+++ contracts/tests/lib/access/AccessControl.spec.ts
@@ -1,8 +1,8 @@
 import '@ton/test-utils'
 
 import { crc32 } from 'zlib'
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox'
-import { Opcodes } from '../../../wrappers/lib/access/AccessControl'
+import { Blockchain } from '@ton/sandbox'
+import { opcodes } from '../../../wrappers/lib/access/AccessControl'
 
 describe('AccessControl', () => {
   let blockchain: Blockchain
@@ -17,8 +17,8 @@ describe('AccessControl', () => {
     const computed = crc32('some_str')
     expect(computed).toBe(0xef3af4b0)
 
-    expect(Opcodes.GrantRole).toBe(0x95cd540f)
-    expect(Opcodes.RevokeRole).toBe(0x969b0db9)
-    expect(Opcodes.RenounceRole).toBe(0x39452c46)
+    expect(opcodes.GrantRole).toBe(0x95cd540f)
+    expect(opcodes.RevokeRole).toBe(0x969b0db9)
+    expect(opcodes.RenounceRole).toBe(0x39452c46)
   })
 })
