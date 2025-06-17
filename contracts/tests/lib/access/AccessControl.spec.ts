6b26decae089d37b48425c1611de80e75960822c
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-06-17 13:19:14 +0200

    Add 'AccessControl_RenounceRole' msg handler

--- contracts/tests/lib/access/AccessControl.spec.ts
+++ contracts/tests/lib/access/AccessControl.spec.ts
@@ -15,9 +15,10 @@ describe('AccessControl', () => {
   it('Should compute crc32 opcodes', async () => {
     // `const op = stringCrc32("some_str")` = 4013618352 = 0xEF3AF4B0
     const computed = crc32('some_str')
-    expect(computed).toBe(0xEF3AF4B0)
+    expect(computed).toBe(0xef3af4b0)
 
     expect(Opcodes.GrantRole).toBe(0x95cd540f)
     expect(Opcodes.RevokeRole).toBe(0x969b0db9)
+    expect(Opcodes.RenounceRole).toBe(0x39452c46)
   })
 })
