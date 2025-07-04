91852d8d967f785e928e86fa74df02b307d85d25
vicente <vicentevieytes@hotmail.com>
2025-07-04 18:07:48 -0300

    implement wrappers

---
+++ contracts/wrappers/examples/ocr/OCR3Base.ts
@@ -0,0 +1,73 @@
+import {
+  Address,
+  beginCell,
+  Builder,
+  Cell,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+} from '@ton/core'
+import { newOCR3BaseCell, OCR3Base, ReportContext, SignatureEd25519 } from '../../libraries/ocr/MultiOCR3Base'
+import { asSnakeData } from '../../../tests/utils'
+
+
+export function ocr3BaseExampleStorage(): Cell {
+  const builder = beginCell()
+    .storeRef(newOCR3BaseCell(1)) //using dummy chainId 1
+  return builder.endCell()
+}
+
+export class OCR3BaseExample extends OCR3Base {
+  constructor(
+    readonly address: Address,
+    readonly init?: { code: Cell; data: Cell },
+  ) {
+    super()
+  }
+
+  static create(code: Cell, workchain = 0) {
+    const data = ocr3BaseExampleStorage()
+    const init = { code, data }
+    return new OCR3BaseExample(
+      contractAddress(workchain, init),
+      init
+    )
+  }
+
+  async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    await provider.internal(via, {
+      value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell().endCell(),
+    })
+  }
+
+  async sendTransmit(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      value: bigint,
+      queryId?: number,
+      ocrPluginType: number,
+      reportContext: ReportContext,
+      report: Cell,
+      signatures: SignatureEd25519[]
+    }) {
+      await provider.internal(via, {
+        value: opts.value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell()
+          .storeUint(0x00000001, 32)//opcode
+          .storeUint(opts.ocrPluginType, 16)
+          .storeUint(opts.reportContext.configDigest, 256)
+          .storeUint(opts.reportContext.sequenceBytes, 64)
+          .storeRef(opts.report)
+          .storeRef(asSnakeData<SignatureEd25519>(
+            opts.signatures,
+            (item) => new Builder().storeUint(item.r, 256).storeUint(item.s, 256)
+          ))
+          .endCell()
+      })
+    }
+}
--- contracts/wrappers/libraries/ocr/MultiOCR3Base.ts
+++ contracts/wrappers/libraries/ocr/MultiOCR3Base.ts
@@ -1,36 +1,86 @@
 import {
   Address,
   beginCell,
+  Builder,
   Cell,
-  Contract,
   ContractProvider,
+  Dictionary,
   Sender,
   SendMode,
-  Slice,
 } from '@ton/core'
-import { SandboxContract, SendMessageResult } from '@ton/sandbox'
 
 import { crc32 } from 'zlib'
+import { asSnakeData } from '../../../tests/utils'
 
 export const Opcodes = {
   //TODO: OP_SET_OCR3_CONFIG: crc32('Ownable2Step_TransferOwnership'),
   OP_SET_OCR3_CONFIG: 0x000000001,
 }
 
-export type OCR3Config = {
-  
+export type ReportContext = {
+  configDigest: bigint,
+  sequenceBytes: number
 }
 
-export class MultiOCR3Base {
+export type OCR3Config = {
+  configInfo: ConfigInfo,
+  signers: bigint[], // public keys
+  transmitters: Address[]
+}
 
+export type ConfigInfo = {
+  configDigest: bigint,
+  bigF: number,
+  n: number,
+  isSignatureVerificationEnabled: boolean,
+}
+
+export type SignatureEd25519 = {
+  r: bigint,
+  s: bigint
+}
+
+export function newOCR3BaseCell(chainId: number): Cell {
+    return beginCell()
+    .storeUint(chainId, 8)
+    .storeDict(Dictionary.empty())
+    .storeUint(16, 16)
+    .storeDict(Dictionary.empty())
+    .storeUint(16, 16)
+    .storeDict(Dictionary.empty())
+    .endCell()
+  }
+
+export class OCR3Base {
   async sendSetOCR3Config(
     provider: ContractProvider,
     via: Sender,
-    opts {
+    opts: {
       value: bigint,
       queryId?: number,
+      ocrPluginType: number,
       ocr3Config: OCR3Config,
     }) {
-      
-    }
+      await provider.internal(via, {
+        value: opts.value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell()
+          .storeUint(Opcodes.OP_SET_OCR3_CONFIG, 32)
+          .storeUint(opts.queryId ?? 0, 64)
+          .storeUint(opts.ocr3Config.configInfo.configDigest, 256)
+          .storeUint(opts.ocrPluginType, 16)
+          .storeUint(opts.ocr3Config.configInfo.bigF, 8)
+          .storeUint(opts.ocr3Config.configInfo.isSignatureVerificationEnabled ? -1 : 0, 1)
+          .storeRef(asSnakeData<bigint>(
+            opts.ocr3Config.signers,
+            (item) => new Builder().storeUint(item, 256)
+          ))
+          .storeRef(asSnakeData<Address>(
+            opts.ocr3Config.transmitters,
+            (item) => new Builder().storeAddress(item)
+          ))
+          .endCell(),
+      })
+  }
+}
  
