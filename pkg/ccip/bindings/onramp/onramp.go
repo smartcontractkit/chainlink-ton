28d09a449fbc5b295a053420168f7772f837e32d
Joe Huang <joe.huang@smartcontract.com>
2025-07-09 21:24:44 -0500

    update comments

--- pkg/ccip/bindings/onramp/onramp.go
+++ pkg/ccip/bindings/onramp/onramp.go
@@ -44,6 +44,7 @@ type CCIPSend struct {
 	ExtraArgs                *cell.Cell                    `tlb:"^"` // four bytes tag + GenericExtraArgsV2 or SVMExtraArgsV1
 }
 
+// DestChainConfig represents the configuration for a destination chain in the CCIP system.
 type DestChainConfig struct {
 	Router           *address.Address `tlb:"addr"`
 	SequenceNumber   uint64           `tlb:"## 64"`
@@ -51,20 +52,23 @@ type DestChainConfig struct {
 	AllowedSender    *cell.Dictionary `tlb:"dict 267"` // it's not documented anywhere, but the address in cell uses 267 bits
 }
 
+// DynamicConfig holds the dynamic configuration for the CCIP system, including fee quoter, fee aggregator, and allow list admin.
 type DynamicConfig struct {
 	FeeQuoter      *address.Address `tlb:"addr"`
 	FeeAggregator  *address.Address `tlb:"addr"`
 	AllowListAdmin *address.Address `tlb:"addr"`
 }
 
+// Ownable2Step represents a two-step ownership structure, where an owner can set a pending owner.
 type Ownable2Step struct {
 	Owner        *address.Address `tlb:"addr"`
 	PendingOwner *address.Address `tlb:"maybe addr"` // PendingOwner is optional
 }
 
+// Storage represents the storage structure for the CCIP onramp contract.
 type Storage struct {
 	Ownable          Ownable2Step                    `tlb:"^"`
 	ChainSelector    uint64                          `tlb:"## 64"`
 	Config           common.SnakeData[DynamicConfig] `tlb:"^"`
-	DestChainConfigs *cell.Dictionary                `tlb:"dict 64"` // TODO check if this is correct, on-chain contract uses Map<uint64> -> DestChainConfig
+	DestChainConfigs *cell.Dictionary                `tlb:"dict 64"`
 }
