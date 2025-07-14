3e64069f7111f92315a4d09b5f1e682409e31b8b
Joe Huang <joe.huang@smartcontract.com>
2025-07-14 17:46:24 -0500

    need testing

--- ops/ccip_onramp/op_deploy.go
+++ ops/ccip_onramp/op_deploy.go
@@ -1,9 +1,16 @@
 package ccip_onramp
 
 import (
+	"fmt"
+
 	"github.com/Masterminds/semver/v3"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	test_utils "github.com/smartcontractkit/chainlink-ton/integration-tests/utils"
 	"github.com/smartcontractkit/chainlink-ton/ops"
+	"github.com/smartcontractkit/chainlink-ton/pkg/ton/tracetracking"
+	"github.com/smartcontractkit/chainlink-ton/pkg/ton/wrappers"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/tvm/cell"
 )
 
 var DeployOnRampOp = operations.NewOperation(
@@ -15,7 +22,22 @@ var DeployOnRampOp = operations.NewOperation(
 
 func deployOnRamp(b operations.Bundle, deps ops.TonDeps, in ops.OpTxInput[DeployCCIPOnrampInput]) (ops.OpTxResult[DeployCCIPOnrampSeqOutput], error) {
 	output := ops.OpTxResult[DeployCCIPOnrampSeqOutput]{}
-	// TODO : Implement the deployment logic for the OnRamp contracts
 
+	// TODO wrap the code cell creation somewhere
+	CounterContractPath := test_utils.GetBuildDir("Onramp.compiled.json")
+	codeCell, err := wrappers.ParseCompiledContract(CounterContractPath)
+	if err != nil {
+		return output, fmt.Errorf("failed to compile contract: %w", err)
+	}
+
+	conn := tracetracking.NewSignedAPIClient(deps.TonChain.Client, *deps.TonChain.Wallet)
+
+	// TODO replace with the actuall cell using onramp gobinding https://github.com/smartcontractkit/chainlink-ton/pull/68
+	contract, err := wrappers.Deploy(&conn, codeCell, cell.BeginCell().EndCell(), tlb.MustFromTON("1"))
+	if err != nil {
+		return output, fmt.Errorf("failed to deploy onramp contract: %w", err)
+	}
+
+	output.Objects.CCIPAddress = contract.Address
 	return output, nil
 }
