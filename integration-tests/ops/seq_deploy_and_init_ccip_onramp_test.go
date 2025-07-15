dd6a7d7862d7f4f87ac162d18871cfe1dddea16f
Joe Huang <joe.huang@smartcontract.com>
2025-07-15 17:23:24 -0500

    refactor

--- integration-tests/ops/seq_deploy_and_init_ccip_onramp_test.go
+++ integration-tests/ops/seq_deploy_and_init_ccip_onramp_test.go
@@ -10,14 +10,6 @@ import (
 	"github.com/smartcontractkit/chainlink-ton/ops/ccip_onramp"
 )
 
-func TestDeployAndInitCCIPOnrampOp(t *testing.T) {
-	t.Parallel()
-
-	env := setupEnv(t)
-	_, err := cld_ops.ExecuteOperation(env.bundle, ccip_onramp.DeployOnRampOp, env.deps, ops.OpTxInput[ccip_onramp.DeployCCIPOnrampInput]{})
-	require.NoError(t, err, "failed to deploy Onramp CCIP Package with DeployOnRampOp")
-}
-
 func TestDeployAndInitCCIPOnrampSequence(t *testing.T) {
 	t.Parallel()
 	env := setupEnv(t)
