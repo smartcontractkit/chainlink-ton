9877211b941472ae3b8e2cb96c8719a05746c9b5
Joe Huang <joe.huang@smartcontract.com>
2025-07-28 17:23:09 -0500

    update test

--- integration-tests/ops/seq_deploy_and_init_ccip_test.go
+++ integration-tests/ops/seq_deploy_and_init_ccip_test.go
@@ -4,21 +4,13 @@ import (
 	"testing"
 
 	cld_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
-	"github.com/smartcontractkit/chainlink-ton/ops"
-	"github.com/smartcontractkit/chainlink-ton/ops/ccip_onramp"
-	"github.com/smartcontractkit/chainlink-ton/ops/ccip_router"
+	"github.com/smartcontractkit/chainlink-ton/ops/ccip/sequence"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDeployAndInitCCIPOnrampSequence(t *testing.T) {
 	t.Parallel()
 	env := setupEnv(t)
-	_, err := cld_ops.ExecuteSequence(env.bundle, ccip_onramp.DeployAndInitCCIPOnRampSequence, env.deps, ops.OpTxInput[ccip_onramp.DeployCCIPOnrampInput]{})
+	_, err := cld_ops.ExecuteSequence(env.bundle, sequence.DeployCCIPSequence, env.deps, sequence.DeployCCIPSeqInput{})
 	require.NoError(t, err, "failed to deploy Onramp CCIP Package with DeployAndInitCCIPOnRampSequence")
-}
-func TestDeployAndInitCCIPRouterSequence(t *testing.T) {
-	t.Parallel()
-	env := setupEnv(t)
-	_, err := cld_ops.ExecuteSequence(env.bundle, ccip_router.DeployAndInitCCIPRouterSequence, env.deps, ops.OpTxInput[ccip_router.DeployCCIPRouterInput]{})
-	require.NoError(t, err, "failed to deploy Router CCIP Package with DeployAndInitCCIPOnRampSequence")
 }
