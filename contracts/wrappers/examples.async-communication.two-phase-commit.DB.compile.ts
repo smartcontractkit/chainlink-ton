55c829e0ee65b07dd9e03ec0152ab145c01c084e
vicente <vicentevieytes@hotmail.com>
2025-07-28 15:43:42 -0300

    fix compilation error

--- contracts/wrappers/examples.async-communication.two-phase-commit.DB.compile.ts
+++ contracts/wrappers/examples.async-communication.two-phase-commit.DB.compile.ts
@@ -3,7 +3,7 @@ import { CompilerConfig } from '@ton/blueprint'
 export const compile: CompilerConfig = {
   lang: 'tact',
   target:
-    'contracts/test/examples/async/test-communication/two/test-phase-commit/test/db.tact/test',
+    'contracts/test/examples/async-communication/two-phase-commit/db.tact',
   options: {
     debug: true,
     interfacesGetter: true,
