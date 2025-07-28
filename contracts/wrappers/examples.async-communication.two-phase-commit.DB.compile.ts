c2ef2234361df1595a7cb10a15e6c2f34cf32412
vicente <vicentevieytes@hotmail.com>
2025-07-28 15:48:53 -0300

    fmt

--- contracts/wrappers/examples.async-communication.two-phase-commit.DB.compile.ts
+++ contracts/wrappers/examples.async-communication.two-phase-commit.DB.compile.ts
@@ -2,8 +2,7 @@ import { CompilerConfig } from '@ton/blueprint'
 
 export const compile: CompilerConfig = {
   lang: 'tact',
-  target:
-    'contracts/test/examples/async-communication/two-phase-commit/db.tact',
+  target: 'contracts/test/examples/async-communication/two-phase-commit/db.tact',
   options: {
     debug: true,
     interfacesGetter: true,
