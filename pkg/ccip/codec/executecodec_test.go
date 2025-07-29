ed04d5688ba7e7fe54cc32ac3b86cb2e14699d91
Joe Huang <joe.huang@smartcontract.com>
2025-07-29 18:15:23 -0500

    add mocks, need to bump common once merged

--- pkg/ccip/codec/executecodec_test.go
+++ pkg/ccip/codec/executecodec_test.go
@@ -9,7 +9,7 @@ import (
 
 	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
-	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3/mocks"
+	"github.com/smartcontractkit/chainlink-ton/mocks/ccipocr3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
