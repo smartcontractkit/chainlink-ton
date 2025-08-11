422a5f29fbee0cd43c795a1dfcf6db15baee9d46
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2025-08-11 09:33:23 +0200

    Init timelock pkg Go bindings

---
+++ pkg/bindings/lib/access/rbac/types.go
@@ -0,0 +1,25 @@
+package rbac
+
+import (
+	"math/big"
+
+	"github.com/xssnick/tonutils-go/address"
+)
+
+// AccessControl data struct, auto-serialized to/from cell.
+type Data struct {
+	// Roles mapping
+	Roles map[uint32]RoleData `tlb:"dict"`
+}
+
+// Internal storage struct for role data
+//
+// Each role has a mapping of accounts that have been granted that role,
+// and an admin role that can manage that role.
+type RoleData struct {
+	adminRole *big.Int `tlb:"## 256"`
+	// Number of members in the role
+	MembersLen uint64 `tlb:"## 64"`
+	// Members of the role, indexed by their address hash.
+	HasRole map[*address.Address]bool `tlb:"dict"` // map<address, slice>
+}
