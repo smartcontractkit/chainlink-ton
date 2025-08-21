7874fb342c0b5457f62e13ad4e4e2d2fb24ec633
Joe Huang <joe.huang@smartcontract.com>
2025-08-21 18:05:34 -0500

    add typescript msg hash script to compare with golang msghasher, right now failed to match, will check

--- pkg/ccip/codec/msghasher_test.go
+++ pkg/ccip/codec/msghasher_test.go
@@ -2,12 +2,15 @@ package codec
 
 import (
 	"context"
+	"encoding/binary"
+	"encoding/hex"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/xssnick/tonutils-go/address"
 
 	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
@@ -150,5 +153,72 @@ func TestMessageHasherV1_ErrorCases(t *testing.T) {
 		_, err := hasher.Hash(ctx, msg)
 		assert.Error(t, err)
 		assert.Contains(t, err.Error(), "failed to decode dest exec data")
+	})
+}
+
+func TestMessageHasherV1_CrossLanguageCompatibility(t *testing.T) {
+	ctx := context.Background()
+	mockExtraDataCodec := new(mocks.SourceChainExtraDataCodec)
+	edc := ccipocr3.ExtraDataCodec(map[string]ccipocr3.SourceChainExtraDataCodec{
+		chainsel.FamilyEVM: mockExtraDataCodec,
+	})
+
+	mockExtraDataCodec.On("DecodeDestExecDataToMap", mock.Anything).Return(map[string]any{
+		"destgasamount": uint32(1000),
+	}, nil)
+	mockExtraDataCodec.On("DecodeExtraArgsToMap", mock.Anything).Return(map[string]any{
+		"gasLimit": big.NewInt(1000),
+	}, nil)
+
+	lg := logger.Test(t)
+	hasher := NewMessageHasherV1(lg, edc)
+
+	t.Run("matches TypeScript generateMessageId", func(t *testing.T) {
+		ac := NewAddressCodec()
+
+		// Use exact same TON address from TypeScript test
+		tonAddr, err := address.ParseAddr("EQDtFpEwcFAEcRe5mLVh2N6C0x-_hJEM7W61_JLnSF74p4q2")
+		require.NoError(t, err)
+
+		receiverAddrBytes, err := ac.AddressStringToBytes(tonAddr.String())
+		require.NoError(t, err)
+
+		// EVM_SENDER_ADDRESS_TEST: 0x1a5fdbc891c5d4e6ad68064ae45d43146d4f9f3a
+		evmSenderBytes, err := hex.DecodeString("1a5fdbc891c5d4e6ad68064ae45d43146d4f9f3a")
+		require.NoError(t, err)
+
+		// Create exact same message as TypeScript test
+		msg := ccipocr3.Message{
+			Header: ccipocr3.RampMessageHeader{
+				MessageID:           [32]byte{},
+				SourceChainSelector: ccipocr3.ChainSelector(909606746561742123),   // CHAINSEL_EVM_TEST_90000001
+				DestChainSelector:   ccipocr3.ChainSelector(13879075125137744094), // CHAINSEL_TON
+				SequenceNumber:      ccipocr3.SeqNum(1),
+				Nonce:               0,
+			},
+			Sender:       ccipocr3.UnknownAddress(hex.EncodeToString(evmSenderBytes)),
+			Data:         []byte{}, // empty cell data
+			Receiver:     receiverAddrBytes,
+			ExtraArgs:    []byte{0, 0, 0, 0}, // empty extra args
+			TokenAmounts: nil,                // no token amounts
+		}
+
+		// Set messageID to 1
+		binary.BigEndian.PutUint64(msg.Header.MessageID[24:], 1)
+
+		hash, err := hasher.Hash(ctx, msg)
+		require.NoError(t, err)
+
+		// Run the TypeScript file to get this value:
+		// chainlink-ton/contracts/tests/ccip/OffRamp.spec.ts  "Test generateMessageId hash compatibility with Go"
+		expectedHashHex := "8a2116dd0f926684493da54fb01944a248eba7f2dcf226252d83156fd9eb494d"
+		expectedHash, err := hex.DecodeString(expectedHashHex)
+		require.NoError(t, err)
+
+		var expectedHashArray [32]byte
+		copy(expectedHashArray[:], expectedHash)
+
+		assert.Equal(t, expectedHashArray, hash,
+			"Go message hasher should produce same hash as TypeScript generateMessageId")
 	})
 }
