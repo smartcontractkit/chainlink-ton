f8cfb955dbb64d2552412561eadd8b19b2f56ccc
vicente <vicentevieytes@hotmail.com>
2025-08-25 15:26:38 -0300

    try to fix nix build

--- contracts/default.nix
+++ contracts/default.nix
@@ -38,7 +38,7 @@
       yarnOfflineCache = pkgs.fetchYarnDeps {
         inherit yarnLock;
         # pin the vendor hash (update using 'pkgs.lib.fakeHash')
-        hash = "sha256-N0IMPdL/9hoeAgU5FFHqtLB+VTkwMgxlTHFSo0AyCSQ=";
+        hash = "sha256-yDBKAXysFjWmV8I3P1M49BCqG0N8q8SH/8G9224bDUk=";
       };
 
       # postPatch script to copy root yarn.lock to the current build directory (and make it writeable)
