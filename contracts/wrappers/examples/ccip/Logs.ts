8e95acaf6df39c96fef1fa6b52e5dba56a5b3182
vicente <vicentevieytes@hotmail.com>
2025-08-26 17:13:28 -0300

    fmt

--- contracts/wrappers/examples/ccip/Logs.ts
+++ contracts/wrappers/examples/ccip/Logs.ts
@@ -1,8 +1,7 @@
-import { crc32 } from "zlib";
-import { Any2TVMMessage } from "../../ccip/OffRamp";
+import { crc32 } from 'zlib'
+import { Any2TVMMessage } from '../../ccip/OffRamp'
 
 export const RECEIVER_CCIP_MESSAGE_RECEIVED = crc32('Receiver_CCIPMessageReceived')
-
 
 export enum LogTypes {
   ReceiverCCIPMessageReceived = RECEIVER_CCIP_MESSAGE_RECEIVED,
