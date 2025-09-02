292c548bef0147911fa55bd9cea6e9fb9c16ad17
Jade Park <jadepark.dev@gmail.com>
2025-09-02 20:19:28 +0100

    feat: replace submodules together

--- scripts/e2e/setup-env.sh
+++ scripts/e2e/setup-env.sh
@@ -8,9 +8,8 @@
 #   4. Starts a new PostgreSQL container for testing.
 #   5. Prepares the Chainlink Core repository by:
 #      - Updating the TON plugin gitRef in plugins.public.yaml with current commit.
-#      - Replacing the chainlink-ton dependency with the local version.
+#      - Replacing all chainlink-ton module dependencies with local versions.
 #      - Tidying Go modules.
-#      - Downloading Go module dependencies.
 #      - Building the 'ccip.test' binary.
 #      - Preparing the test database schema using 'preparetest' cmd.
 #
@@ -25,7 +24,7 @@
 #
 # Notes:
 #   - This script modifies the go.mod file of the specified Chainlink Core directory
-#     to use the local Chainlink TON project.
+#     to use the local Chainlink TON project and its submodules.
 #   - This script updates the TON plugin gitRef in plugins.public.yaml to use the current commit.
 #   - Ensure that the blessed commit in .core_version matches with the Core repository git ref.
 
@@ -205,15 +204,32 @@ log_info "Current chainlink-ton commit: $CURRENT_TON_COMMIT"
     exit 1
   fi
 
+  # replace chainlink-ton dependency with local version at chainlink root
+  log_info "Replacing chainlink-ton dependencies with local version..."
+  
+  # list of chainlink-ton modules that needs to be replaced in core
+  declare -A CHAINLINK_TON_MODULES=(
+    ["github.com/smartcontractkit/chainlink-ton"]="$ROOT_DIR"
+    ["github.com/smartcontractkit/chainlink-ton/deployment"]="$ROOT_DIR/deployment"
+    ["github.com/smartcontractkit/chainlink-ton/integration-tests/utils"]="$ROOT_DIR/integration-tests/utils"
+  )
+  
+  # replace all chainlink-ton modules with local versions
+  for module in "${!CHAINLINK_TON_MODULES[@]}"; do
+    local_path="${CHAINLINK_TON_MODULES[$module]}"
+    log_info "  Replacing $module -> $local_path"
+    go mod edit -replace="$module=$local_path"
+  done
+  
+  go run github.com/jmank88/gomods@v0.1.6 tidy
+  log_info "Go modules updated successfully"
+
   cd "./integration-tests"
   go build -o ccip.test .
 
   # reference: https://github.com/smartcontractkit/chainlink-ccip/blob/main/.github/workflows/ccip-integration-test.yml#L169-L221
   go get github.com/smartcontractkit/chainlink/v2/core/store/cmd/preparetest@develop
   go run github.com/smartcontractkit/chainlink/v2/core/store/cmd/preparetest
-
-  go mod edit -replace="github.com/smartcontractkit/chainlink-ton=$ROOT_DIR"
-  go run github.com/jmank88/gomods@v0.1.6 tidy
 )
 
 log_info "=================================="
@@ -224,7 +240,7 @@ log_info "Please ensure CL_DATABASE_URL is exported in your environment before r
 log_info "export CL_DATABASE_URL=${CL_DATABASE_URL}"
 log_info "=================================="
 log_info "IMPORTANT: Please note this setup makes the following changes to chainlink core:"
-log_info "  1. Adds replace directive: github.com/smartcontractkit/chainlink-ton => ${ROOT_DIR}"
+log_info "  1. Adds replace directives for all chainlink-ton modules to use local versions"
 log_info "  2. Updates TON plugin gitRef in plugins.public.yaml to: ${CURRENT_TON_COMMIT}"
-log_info "This will use your local chainlink-ton code in the core repo."
+log_info "This will use your local chainlink-ton code and submodules in the core repo."
 log_info "=================================="
