8c8cf6adcb56eb2170eb9e5874ceebf388bf728b
Joe Huang <joe.huang@smartcontract.com>
2025-09-04 14:02:30 -0300

    update nix hash

--- cmd/chainlink-ton/default.nix
+++ cmd/chainlink-ton/default.nix
@@ -18,7 +18,7 @@ in
     ];
 
     # pin the vendor hash (update using 'pkgs.lib.fakeHash')
-    vendorHash = "sha256-juuO3JRZYbUn9qj08zuka03EmEKVfVSxLfxPrlTHLhs=";
+    vendorHash = "sha256-S3IHRz/GeybBJFgB9YvYoNMSypQodS6eYG9/I+74h/8=";
 
     # postInstall script to write version and rev to share folder
     postInstall = ''
