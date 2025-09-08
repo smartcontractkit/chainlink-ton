9b939839e1bb892d67c09d5972619e9e0b2fe32e
Jade Park <jadepark.dev@gmail.com>
2025-09-08 22:03:13 +0100

    chore: update default config

--- pkg/logpoller/config.go
+++ pkg/logpoller/config.go
@@ -14,6 +14,6 @@ type Config struct {
 var DefaultConfigSet = Config{
 	PollPeriod:                5 * time.Second,
 	PageSize:                  100,
-	LogPollerStartingLookback: 24 * time.Hour,  // Look back 24 hours on startup
-	BlockTime:                 5 * time.Second, // TON block time is approximately 5 seconds
+	LogPollerStartingLookback: 24 * time.Hour,          // Look back 24 hours on startup
+	BlockTime:                 2500 * time.Millisecond, // TON block time is approximately 2.5 seconds
 }
