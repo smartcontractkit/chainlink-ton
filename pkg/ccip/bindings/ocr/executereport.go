025101b1b610a5a9d26a25fc750b2110f5a76371
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2025-09-19 09:21:31 -0300

    fix: compilation

--- pkg/ccip/bindings/ocr/executereport.go
+++ pkg/ccip/bindings/ocr/executereport.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/tlb"
 	"github.com/xssnick/tonutils-go/tvm/cell"
 
 	"github.com/smartcontractkit/chainlink-ton/pkg/ccip/bindings/common"
