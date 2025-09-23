7f257ff6c40bac1b7332df7af851b0e6a2985631
Joe Huang <joe.huang@smartcontract.com>
2025-09-23 15:58:53 -0500

    experiment with contract build sha

---
+++ deployment/contract_version.go
@@ -0,0 +1,18 @@
+package deployment
+
+import (
+	"os"
+	"strings"
+)
+
+// GetContractBuildSHA returns the most up-to-date build SHA for contracts for the current version, to be used by other workflows/repos
+//
+//	note: the hash of the built artifacts is automatically modified only after a branch gets merged in the main branch as a release
+func GetContractBuildSHA() (string, error) {
+	const shaFile = ".contract_build_sha"
+	data, err := os.ReadFile(shaFile)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(data)), nil
+}
