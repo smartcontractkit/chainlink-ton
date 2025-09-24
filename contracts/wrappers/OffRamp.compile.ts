6a2ba3cf3c8a2ef654a03fc1da211a99bdd96ee7
vicente <vicentevieytes@hotmail.com>
2025-09-24 17:08:08 -0300

    wip: fix compilation errors

--- contracts/wrappers/OffRamp.compile.ts
+++ contracts/wrappers/OffRamp.compile.ts
@@ -2,6 +2,6 @@ import { CompilerConfig } from '@ton/blueprint'
 
 export const compile: CompilerConfig = {
   lang: 'tolk',
-  entrypoint: 'contracts/ccip/offramp.tolk',
+  entrypoint: 'contracts/ccip/offramp/contract.tolk',
   withStackComments: true,
 }
