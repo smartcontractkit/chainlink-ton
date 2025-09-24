b2b0f189632bf1256e830340c978dc30a52a26ff
Joe Huang <joe.huang@smartcontract.com>
2025-09-24 11:12:25 -0500

    refactor and add test

---
+++ deployment/utils/contract_version_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGetContractBuildSHA(t *testing.T) {
+	const shaFile = ".contract_build_sha"
+
+	data, err := os.ReadFile(shaFile)
+	if err != nil {
+		t.Skipf("%s not found, skipping test", shaFile)
+	}
+	expected := strings.TrimSpace(string(data))
+
+	sha, err := GetContractBuildSHA()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if sha != expected {
+		t.Errorf("expected %s, got %s", expected, sha)
+	}
+}
