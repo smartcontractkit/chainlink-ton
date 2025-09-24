5eacf671d25c9155af015b2d0450114461374854
Kristijan <kristijan.rebernisak@gmail.com>
2025-09-24 15:32:30 +0200

    Update error codes mcms/rbac_timelock.tolk

--- contracts/wrappers/mcms/RBACTimelock.ts
+++ contracts/wrappers/mcms/RBACTimelock.ts
@@ -798,22 +798,22 @@ export const DONE_TIMESTAMP = 1n
 export const ERROR_TIMESTAMP = 2n
 
 export enum Errors {
-  SelectorIsBlocked = 101,
-  OperationNotReady = 102,
-  OperationMissingDependency = 103,
-  OperationCanNotBeCancelled = 104,
-  OperationAlreadyScheduled = 105,
-  InsufficientDelay = 106,
+  SelectorIsBlocked = 21300,
+  OperationNotReady,
+  OperationMissingDependency,
+  OperationCanNotBeCancelled,
+  OperationAlreadyScheduled,
+  InsufficientDelay,
   /// Thrown when trying to execute a pending operation while another pending operation is not yet final
-  PendingOperationNotFinal = 107,
+  PendingOperationNotFinal,
   /// Thrown when the provided op.value is insufficient (min required value not met).
-  InsufficientValue = 108,
+  InsufficientValue,
   /// Thrown when trying to submit an error report for an operation that is not done.
-  OperationNotDone = 109,
+  OperationNotDone,
   /// Thrown when trying to initialize the contract more than once.
-  ContractAlreadyInitialized = 110,
+  ContractAlreadyInitialized,
   /// Thrown when trying to call a function on an uninitialized contract.
-  ContractNotInitialized = 111,
+  ContractNotInitialized,
 }
 
 export class ContractClient implements Contract {
