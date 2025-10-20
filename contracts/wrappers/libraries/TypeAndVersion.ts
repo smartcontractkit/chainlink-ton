70f5a9cee45a5ead4682a39886628d6e8c5cebe1
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2025-10-20 16:58:08 -0300

    ref: remove unnecesary getCode and getCodeHash

--- contracts/wrappers/libraries/TypeAndVersion.ts
+++ contracts/wrappers/libraries/TypeAndVersion.ts
@@ -8,13 +8,20 @@ export async function getTypeAndVersion(
 }
 
 export async function getCode(provider: ContractProvider): Promise<Cell> {
-  const result = await provider.get('code', [])
-  return result.stack.readCell()
+  const state = await provider.getState()
+  if (state.state.type !== 'active') {
+    throw new Error('Contract is not active: ' + state.state.type.toString())
+  }
+  if (state.state.code == null) {
+    throw new Error('Contract has no code')
+  }
+  return Cell.fromBoc(state.state.code)[0]
 }
 
 export async function getCodeHash(provider: ContractProvider): Promise<bigint> {
-  const result = await provider.get('codeHash', [])
-  return result.stack.readBigNumber()
+  const code = await getCode(provider)
+  const hash = code.hash()
+  return BigInt('0x' + hash.toString('hex'))
 }
 
 export interface TypeAndVersion {
