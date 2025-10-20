42aaca03ac79d3e7bb1a67cc0621bbf5f277efe4
Joe Huang <joe.huang@smartcontract.com>
2025-10-20 05:36:05 -0500

    use go embed

--- deployment/utils/contract_version.go
+++ deployment/utils/contract_version.go
@@ -1,18 +1,21 @@
 package utils
 
 import (
-	"os"
+	_ "embed"
+	"fmt"
 	"strings"
 )
+
+//go:embed contract_build_sha
+var contractBuildSHA string
 
 // GetContractBuildSHA returns the most up-to-date build SHA for contracts for the current version, to be used by other workflows/repos
 //
 //	note: the hash of the built artifacts is automatically modified only after a branch gets merged in the main branch as a release
 func GetContractBuildSHA() (string, error) {
-	const shaFile = "contract_build_sha"
-	data, err := os.ReadFile(shaFile)
-	if err != nil {
-		return "", err
+	sha := strings.TrimSpace(contractBuildSHA)
+	if sha == "" {
+		return "", fmt.Errorf("contract build SHA not found")
 	}
-	return strings.TrimSpace(string(data)), nil
+	return sha, nil
 }
