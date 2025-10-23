ae13f9e00634f0b455f39c5ade1d4e10831acb15
Joe Huang <joe.huang@smartcontract.com>
2025-10-23 13:27:11 -0500

    add comments

--- pkg/ccip/bindings/common/common.go
+++ pkg/ccip/bindings/common/common.go
@@ -41,8 +41,11 @@ type FetchOptions struct {
 	DestChainSelector uint64
 }
 
+// ConfigFetcher is an interface for fetching and parsing contract configurations.
 type ConfigFetcher interface {
+	// FetchResult fetches the configuration from the contract at the specified block and address.
 	FetchResult(ctx context.Context, client ton.APIClientWrapped, block *ton.BlockIDExt, contractAddr *address.Address, opts *FetchOptions) error
+	// FromResult parses the configuration from the execution result.
 	FromResult(result *ton.ExecutionResult) error
 }
 
