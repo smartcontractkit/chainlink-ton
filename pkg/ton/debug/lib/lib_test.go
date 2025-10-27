4364e12c1d3a6d446be04330038a5a8832ba8b7c
Kristijan <kristijan.rebernisak@gmail.com>
2025-10-27 18:20:18 +0100

    Add TestDecodeJSONMapFromCellIteratively

--- pkg/ton/debug/lib/lib_test.go
+++ pkg/ton/debug/lib/lib_test.go
@@ -50,6 +50,52 @@ func mustToCell(v interface{}) *cell.Cell {
 	return c
 }
 
+// Shared complex test case
+var testMCMSExecuteCell = mustToCell(mcms.Execute{
+	QueryID: 31,
+	Op: mcms.Op{
+		ChainID:  big.NewInt(-14),
+		MultiSig: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+		Nonce:    42,
+		To:       address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+		Value:    tlb.MustFromTON("1.5"),
+		Data: mustToCell(timelock.ScheduleBatch{
+			QueryID: 31,
+			Calls: common.SnakeRef[timelock.Call]{
+				timelock.Call{
+					Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+					Value:  tlb.MustFromTON("0.5").Nano(),
+					Data: mustToCell(Bar{
+						Val: big.NewInt(55555555),
+					}),
+				},
+				timelock.Call{
+					Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+					Value:  tlb.MustFromTON("1.0").Nano(),
+					Data: mustToCell(Baz{
+						Val: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+					}),
+				},
+				timelock.Call{
+					Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+					Value:  tlb.MustFromTON("1.5").Nano(),
+					Data: mustToCell(wallet.AskToTransfer{
+						QueryID:     0,
+						Amount:      tlb.MustFromTON("0.02"),
+						Destination: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
+						// CustomPayload:    cell.BeginCell().EndCell(), // default for *cell.Cell
+						ForwardPayload:   mustToCell(Foo{Any: mustToCell(Baz{Val: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8")})}),
+						ForwardTonAmount: tlb.MustFromTON("0.01"),
+					}),
+				},
+			},
+			Predecessor: big.NewInt(-1),
+			Salt:        big.NewInt(1337),
+			Delay:       10000,
+		}),
+	},
+})
+
 func TestDecodeJSONMapFromCell(t *testing.T) {
 	tests := []struct {
 		name      string
@@ -173,51 +219,8 @@ func TestDecodeJSONMapFromCell(t *testing.T) {
 			expectErr: false,
 		},
 		{
-			name: "Decode MCMS Execute > Timelock ScheduleBatch > Op[]s with Bar and Baz in payload",
-			cell: mustToCell(mcms.Execute{
-				QueryID: 31,
-				Op: mcms.Op{
-					ChainID:  big.NewInt(-14),
-					MultiSig: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-					Nonce:    42,
-					To:       address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-					Value:    tlb.MustFromTON("1.5"),
-					Data: mustToCell(timelock.ScheduleBatch{
-						QueryID: 31,
-						Calls: common.SnakeRef[timelock.Call]{
-							timelock.Call{
-								Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-								Value:  tlb.MustFromTON("0.5").Nano(),
-								Data: mustToCell(Bar{
-									Val: big.NewInt(55555555),
-								}),
-							},
-							timelock.Call{
-								Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-								Value:  tlb.MustFromTON("1.0").Nano(),
-								Data: mustToCell(Baz{
-									Val: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-								}),
-							},
-							timelock.Call{
-								Target: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-								Value:  tlb.MustFromTON("1.5").Nano(),
-								Data: mustToCell(wallet.AskToTransfer{
-									QueryID:     0,
-									Amount:      tlb.MustFromTON("0.02"),
-									Destination: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8"),
-									// CustomPayload:    cell.BeginCell().EndCell(), // default for *cell.Cell
-									ForwardPayload:   mustToCell(Foo{Any: mustToCell(Baz{Val: address.MustParseAddr("EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8")})}),
-									ForwardTonAmount: tlb.MustFromTON("0.01"),
-								}),
-							},
-						},
-						Predecessor: big.NewInt(-1),
-						Salt:        big.NewInt(1337),
-						Delay:       10000,
-					}),
-				},
-			}),
+			name:     "Decode MCMS Execute > Timelock ScheduleBatch > Op[]s with Bar and Baz in payload",
+			cell:     testMCMSExecuteCell,
 			wantType: "Execute",
 			wantMap: map[string]interface{}{
 				"QueryID": float64(31),
@@ -274,11 +277,194 @@ func TestDecodeJSONMapFromCell(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotType, normMap, err := DecodeTLBValToJSON(tt.cell, TLBs)
+			gotType, norm, err := DecodeTLBValToJSON(tt.cell, TLBs)
 			require.NoError(t, err, "failed to DecodeTLBValToJSON")
 
 			var gotMap map[string]interface{}
-			rawBytes, err := json.Marshal(normMap)
+			rawBytes, err := json.Marshal(norm)
+			require.NoError(t, err, "failed to marshal decoded message to JSON")
+			err = json.Unmarshal(rawBytes, &gotMap)
+			require.NoError(t, err, "failed to unmarshal decoded message JSON to map")
+
+			if (err != nil) != tt.expectErr {
+				t.Errorf("DecodeTLBValToJSON() error = %v, expectErr %v", err, tt.expectErr)
+			}
+			if gotType != tt.wantType {
+				t.Errorf("DecodeTLBValToJSON() gotType = %v, want %v", gotType, tt.wantType)
+			}
+			if !reflect.DeepEqual(gotMap, tt.wantMap) {
+				t.Errorf("DecodeTLBValToJSON() gotMap = %v, want %v", gotMap, tt.wantMap)
+			}
+		})
+	}
+}
+
+// double decoding test (1) domain TLBs or error, (2) context/payload TLBs
+func TestDecodeJSONMapFromCellIteratively(t *testing.T) {
+	tests := []struct {
+		name         string
+		cell         *cell.Cell
+		tlbsMain     map[uint64]interface{}
+		tlbsPayloads map[uint64]interface{}
+		wantType     string
+		wantMap      map[string]interface{}
+		expectErr    bool
+	}{
+		{
+			name: "Decode MCMS Execute > Timelock ScheduleBatch > Ops - payload TLBs available",
+			cell: testMCMSExecuteCell,
+			tlbsMain: MustNewTLBMap([]interface{}{
+				mcms.Execute{},
+			}),
+			tlbsPayloads: MustNewTLBMap([]interface{}{
+				Foo{},
+				Bar{},
+				Baz{},
+				wallet.AskToTransfer{},
+				timelock.ScheduleBatch{},
+			}),
+			wantType: "map[string]interface {}",
+			wantMap: map[string]interface{}{
+				"QueryID": float64(31),
+				"Op": map[string]interface{}{
+					"ChainID":  float64(-14),
+					"MultiSig": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Nonce":    float64(42),
+					"To":       "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Value":    "1500000000",
+					"Data": map[string]interface{}{
+						"QueryID": float64(31),
+						"Calls": []interface{}{
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(500000000),
+								"Data": map[string]interface{}{
+									"Val": float64(55555555),
+								},
+							},
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(1000000000),
+								"Data": map[string]interface{}{
+									"Val": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								},
+							},
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(1500000000),
+								"Data": map[string]interface{}{
+									"QueryID":       float64(0),
+									"Amount":        "20000000",
+									"Destination":   "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+									"CustomPayload": "te6cckEBAgEAMwABDzmJaAAAAAAMAQBLAAAAA4AAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG1B7fNdk",
+									"ForwardPayload": map[string]interface{}{
+										"Any": map[string]interface{}{
+											"Val": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+										},
+									},
+									"ForwardTonAmount":    "10000000",
+									"ResponseDestination": "NONE",
+								},
+							},
+						},
+						"Predecessor": float64(-1),
+						"Salt":        float64(1337),
+						"Delay":       float64(10000),
+					},
+				},
+				"Proof": []any{},
+			},
+		},
+		{
+			name: "Decode MCMS Execute > Timelock ScheduleBatch > Ops - payload TLBs (some) NOT available",
+			cell: testMCMSExecuteCell,
+			tlbsMain: MustNewTLBMap([]interface{}{
+				mcms.Execute{},
+				wallet.AskToTransfer{},
+			}),
+			tlbsPayloads: MustNewTLBMap([]interface{}{
+				Foo{},
+				Bar{},
+				Baz{},
+				timelock.ScheduleBatch{},
+			}),
+			wantType: "map[string]interface {}",
+			wantMap: map[string]interface{}{
+				"QueryID": float64(31),
+				"Op": map[string]interface{}{
+					"ChainID":  float64(-14),
+					"MultiSig": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Nonce":    float64(42),
+					"To":       "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Value":    "1500000000",
+					"Data": map[string]interface{}{
+						"QueryID": float64(31),
+						"Calls": []interface{}{
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(500000000),
+								"Data": map[string]interface{}{
+									"Val": float64(55555555),
+								},
+							},
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(1000000000),
+								"Data": map[string]interface{}{
+									"Val": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								},
+							},
+							map[string]interface{}{
+								"Target": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+								"Value":  float64(1500000000),
+								"Data":   "te6cckEBAgEAZQABcw+KfqUAAAAAAAAAAEATEtAIAAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG0DmJaAAAAAAMBAEsAAAADgABtbrdDaTpRJq8NdPXIgcbPqeoYpCTt/LPB0uV2rZgbUNAF680=",
+							},
+						},
+						"Predecessor": float64(-1),
+						"Salt":        float64(1337),
+						"Delay":       float64(10000),
+					},
+				},
+				"Proof": []any{},
+			},
+		},
+		{
+			name: "Decode MCMS Execute > Timelock ScheduleBatch > Ops - payload TLBs (most) NOT available",
+			cell: testMCMSExecuteCell,
+			tlbsMain: MustNewTLBMap([]interface{}{
+				mcms.Execute{},
+			}),
+			tlbsPayloads: MustNewTLBMap([]interface{}{
+				Foo{},
+				Bar{},
+				Baz{},
+			}),
+			wantType: "map[string]interface {}",
+			wantMap: map[string]interface{}{
+				"QueryID": float64(31),
+				"Op": map[string]interface{}{
+					"ChainID":  float64(-14),
+					"MultiSig": "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Nonce":    float64(42),
+					"To":       "EQADa3W6G0nSiTV4a6euRA42fU9QxSEnb-WeDpcrtWzA2jM8",
+					"Value":    "1500000000",
+					"Data":     "te6cckECCAEAAZoAAagJRxj0AAAAAAAAAB///////////////////////////////////////////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAU5AAAAAAAAJxABAwACAwQBg4AAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADuaygEAUBg4AAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHc1lAEAcBg4AAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALLQXgEAYAEAAAAAIDT7XjAXMPin6lAAAAAAAAAABAExLQCAAG1ut0NpOlEmrw109ciBxs+p6hikJO38s8HS5XatmBtA5iWgAAAAADBwBLAAAAA4AAbW63Q2k6USavDXT1yIHGz6nqGKQk7fyzwdLldq2YG1BAjK8m",
+				},
+				"Proof": []any{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, norm, err := DecodeTLBValToJSON(tt.cell, tt.tlbsMain)
+			require.NoError(t, err, "failed to DecodeTLBValToJSON - tlbs main")
+
+			gotType, norm, err := DecodeTLBValToJSON(norm, tt.tlbsPayloads)
+			require.NoError(t, err, "failed to DecodeTLBValToJSON - tlbs payloads")
+
+			var gotMap map[string]interface{}
+			rawBytes, err := json.Marshal(norm)
 			require.NoError(t, err, "failed to marshal decoded message to JSON")
 			err = json.Unmarshal(rawBytes, &gotMap)
 			require.NoError(t, err, "failed to unmarshal decoded message JSON to map")
