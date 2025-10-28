24908240fd05ab3761c62448b69fb4940b429e37
vicente <vicentevieytes@hotmail.com>
2025-10-28 19:06:42 -0300

    fmt view/common.go

--- deployment/view/common.go
+++ deployment/view/common.go
@@ -9,7 +9,7 @@ import (
 const (
 	VersionGetter                   = "typeAndVersion"
 	DestChainsGetter                = "destChainSelectors"
-	SourceChainsGetter		= "sourceChainSelectors"
+	SourceChainsGetter              = "sourceChainSelectors"
 	DestChainConfigGetter           = "destChainConfig"
 	SrcChainConfigGetter            = "sourceChainConfig"
 	ConfigGetter                    = "config"
