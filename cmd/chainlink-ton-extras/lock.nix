8eb094c751fdfca60eebd4436e39a53a2c4147cf
Kristijan <kristijan.rebernisak@gmail.com>
2025-11-04 16:18:06 +0100

    Bump vendor hash

--- cmd/chainlink-ton-extras/lock.nix
+++ cmd/chainlink-ton-extras/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `nix-lock-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton-extras = "sha256-7akjNApf3pyX0u8hSwulimpsHJyYW/LvlwSuAVyen/Q=";
+  chainlink-ton-extras = "sha256-Qp23zuGQmZrNGly0QyyXqMUBxvH5OwtNAg+TriPn3TQ=";
 }
