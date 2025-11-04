7f5e78aed9b7cc96c6a77fbf31f68b0d2d743184
Jade Park <jadepark.dev@gmail.com>
2025-11-04 23:11:24 +0000

    chore: clean up

--- pkg/logpoller/backend/loader/account/account.go
+++
@@ -1,9 +0,0 @@
-// Package account provides backward compatibility for the old import path.
-// Deprecated: Use github.com/smartcontractkit/chainlink-ton/pkg/logpoller/loader instead.
-package account
-
-import "github.com/smartcontractkit/chainlink-ton/pkg/logpoller/loader"
-
-// Re-export New function from the new location
-var New = loader.New
-
