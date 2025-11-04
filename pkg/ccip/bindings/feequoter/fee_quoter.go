35e107354bdf8b3af3a485e38cd9710374b31be1
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2025-11-04 12:48:40 -0300

    rm: unsued bindings

--- pkg/ccip/bindings/feequoter/fee_quoter.go
+++ pkg/ccip/bindings/feequoter/fee_quoter.go
@@ -20,13 +20,11 @@ const (
 
 // Fee Quoter opcodes
 const (
-	OpcodeUpdatePrices                     = 0x20000001
-	OpcodeUpdateFeeTokens                  = 0xD0984986
-	OpcodeUpdateTokenTransferFeeConfigs    = 0xB2826316
-	OpcodeUpdateDestChainConfigs           = 0x29950BAA
-	OpcodeFeeQuoterGetValidatedFee         = 0x7496FF56
-	OpcodeFeeQuoterMessageValidated        = 0xCBC4AF76
-	OpcodeFeeQuoterMessageValidationFailed = 0x0F756150
+	OpcodeUpdatePrices                  = 0x20000001
+	OpcodeUpdateFeeTokens               = 0xD0984986
+	OpcodeUpdateTokenTransferFeeConfigs = 0xB2826316
+	OpcodeUpdateDestChainConfigs        = 0x29950BAA
+	OpcodeFeeQuoterGetValidatedFee      = 0x7496FF56
 )
 
 //go:generate go run golang.org/x/tools/cmd/stringer@v0.38.0 -type=ExitCode
