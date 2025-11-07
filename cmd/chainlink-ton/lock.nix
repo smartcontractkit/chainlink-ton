7157d8cd47928e3ac8b0102e6243ed5d2495df5c
Oliver Townsend <oliver.townsend@smartcontract.com>
2025-11-07 14:18:19 -0800

    update vendor hash

--- cmd/chainlink-ton/lock.nix
+++ cmd/chainlink-ton/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `nix-lock-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton = "sha256-tkI5csX677bP3rRH13dvKZzOsPumpfI12Ez/7MDED4s=";
+  chainlink-ton = "sha256-iuLL/v89uyAvTOvF2GyrQInB/8SQyskhhzdtOqJiISE=";
 }
