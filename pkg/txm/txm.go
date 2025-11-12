cc2a88b83bf8ccb044197a24ee79771739640bfe
vicente <vicentevieytes@hotmail.com>
2025-11-12 18:22:14 -0300

    refactor: move Costs structs to their corresponding contracts

--- pkg/txm/txm.go
+++ pkg/txm/txm.go
@@ -253,8 +253,8 @@ func (t *Txm) broadcastWithRetry(ctx context.Context, tx *Tx, msg *wallet.Messag
 			if err != nil {
 				t.logger.Errorw("failed to wait for trace: %w", err)
 			}
-			t.logger.Infof("Msg tree trace:\n%s\n", debug.NewDebuggerTreeTrace(nil).DumpReceived(receivedMessage))
-			t.logger.Infof("Msg sequence diagram:\n%s\n", debug.NewDebuggerSequenceTrace(nil, sequenceDiagram.OutputFmtURL).DumpReceived(receivedMessage))
+			t.logger.Debugf("Msg tree trace:\n%s\n", debug.NewDebuggerTreeTrace(nil).DumpReceived(receivedMessage))
+			t.logger.Debugf("Msg sequence diagram:\n%s\n", debug.NewDebuggerSequenceTrace(nil, sequenceDiagram.OutputFmtURL).DumpReceived(receivedMessage))
 			break
 		}
 
