eb886d3aa39be4d185d494de1329835ecfd885b1
vicente <vicentevieytes@hotmail.com>
2025-11-13 18:33:04 -0300

    fix router test is not setting up correctly fee quoter prices

--- contracts/tests/ccip/CCIPRouter.spec.ts
+++ contracts/tests/ccip/CCIPRouter.spec.ts
@@ -208,6 +208,7 @@ describe('Router', () => {
               gasPricesUpdates: [],
               tokenPricesUpdates: [{ token: TEST_TOKEN_ADDR, price: BigInt(123e36) }],
             },
+              sendExcessesTo: undefined
           },
         })
         expect(result.transactions).toHaveTransaction({
