649d06a3abe348c059990d7ea6688832b8d25617
Oliver Townsend <oliver.townsend@smartcontract.com>
2025-11-17 16:42:32 -0800

    commit codec logs

--- pkg/ccip/codec/commitcodec.go
+++ pkg/ccip/codec/commitcodec.go
@@ -10,6 +10,7 @@ import (
 	"github.com/xssnick/tonutils-go/tlb"
 	"github.com/xssnick/tonutils-go/tvm/cell"
 
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	"github.com/smartcontractkit/chainlink-ton/pkg/ccip/bindings/common"
@@ -29,6 +30,18 @@ func NewCommitPluginCodecV1() cciptypes.CommitPluginCodec {
 }
 
 func (cr *commitPluginCodecV1) Encode(ctx context.Context, report cciptypes.CommitPluginReport) ([]byte, error) {
+	// create logger here to log unpacking process
+	lggr, err := logger.New()
+	if err != nil {
+		return nil, fmt.Errorf("cannot create logger: %w", err)
+	}
+
+	lggr.Debugw("OGT commitcodec.Encode START",
+		"numTokenPriceUpdates", len(report.PriceUpdates.TokenPriceUpdates),
+		"numGasPriceUpdates", len(report.PriceUpdates.GasPriceUpdates),
+		"numBlessedMerkleRoots", len(report.BlessedMerkleRoots),
+		"numUnblessedMerkleRoots", len(report.UnblessedMerkleRoots))
+
 	tpuSlice := make([]ocr.TokenPriceUpdate, len(report.PriceUpdates.TokenPriceUpdates))
 	for i, tpu := range report.PriceUpdates.TokenPriceUpdates {
 		addr, err := address.ParseAddr(string(tpu.TokenID))
@@ -51,19 +64,32 @@ func (cr *commitPluginCodecV1) Encode(ctx context.Context, report cciptypes.Comm
 			return nil, fmt.Errorf("empty gas price for chain selector %d", gpu.ChainSel)
 		}
 
+		lggr.Debugw("OGT commitcodec.Encode unpacking gas price",
+			"index", i,
+			"chainSel", gpu.ChainSel,
+			"packedGasPrice", gpu.GasPrice.Int.String())
+
 		// The GasPrice is packed as: (DA << 112) | Exec by the plugin.
 		// We need to unpack it into two separate 112-bit fields for the TON onchain struct.
 		execFee, daFee := feequoter.UnpackGasPrice(gpu.GasPrice.Int)
 
-		// DEBUG: Log what we're unpacking
-		fmt.Printf("OGT DEBUG Encode: chain=%d, packed=%s, exec=%s, da=%s\n",
-			gpu.ChainSel, gpu.GasPrice.Int.String(), execFee.String(), daFee.String())
+		lggr.Debugw("OGT commitcodec.Encode unpacked gas price",
+			"index", i,
+			"chainSel", gpu.ChainSel,
+			"execFee", execFee.String(),
+			"daFee", daFee.String())
 
 		gpuSlice[i] = ocr.GasPriceUpdate{
 			DestChainSelector:        uint64(gpu.ChainSel),
 			ExecutionGasPrice:        execFee,
 			DataAvailabilityGasPrice: daFee,
 		}
+
+		lggr.Debugw("OGT commitcodec.Encode created GasPriceUpdate struct",
+			"index", i,
+			"chainSel", gpuSlice[i].DestChainSelector,
+			"ExecutionGasPrice_field", gpuSlice[i].ExecutionGasPrice.String(),
+			"DataAvailabilityGasPrice_field", gpuSlice[i].DataAvailabilityGasPrice.String())
 	}
 
 	mkSlice := make([]ocr.MerkleRoot, len(report.BlessedMerkleRoots))
@@ -96,25 +122,57 @@ func (cr *commitPluginCodecV1) Encode(ctx context.Context, report cciptypes.Comm
 		MerkleRoots: append(mkSlice, unblessedMkSlice...),
 	}
 
+	lggr.Debugw("OGT commitcodec.Encode about to call tlb.ToCell",
+		"numGasPriceUpdates", len(cellReport.PriceUpdates.GasPriceUpdates))
+
 	c, err := tlb.ToCell(cellReport)
 	if err != nil {
+		lggr.Errorw("OGT commitcodec.Encode tlb.ToCell FAILED", "err", err)
 		return nil, fmt.Errorf("cannot encode commit report to cell: %w", err)
 	}
 
+	lggr.Debugw("OGT commitcodec.Encode tlb.ToCell succeeded",
+		"cellBits", c.BitsSize(),
+		"cellRefs", c.RefsNum())
+
 	// Serialize the cell to bytes
-	return c.ToBOC(), nil
+	boc := c.ToBOC()
+	
+	lggr.Debugw("OGT commitcodec.Encode completed",
+		"bocLength", len(boc))
+	
+	return boc, nil
 }
 
 func (cr *commitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (cciptypes.CommitPluginReport, error) {
+	lggr, err := logger.New()
+	if err != nil {
+		return cciptypes.CommitPluginReport{}, fmt.Errorf("cannot create logger: %w", err)
+	}
+
+	lggr.Debugw("OGT commitcodec.Decode START",
+		"bocLength", len(bytes))
+
 	c, err := cell.FromBOC(bytes)
 	if err != nil {
+		lggr.Errorw("OGT commitcodec.Decode cell.FromBOC FAILED", "err", err)
 		return cciptypes.CommitPluginReport{}, fmt.Errorf("cannot decode BOC: %w", err)
 	}
 
+	lggr.Debugw("OGT commitcodec.Decode cell.FromBOC succeeded",
+		"cellBits", c.BitsSize(),
+		"cellRefs", c.RefsNum())
+
 	var report ocr.CommitReport
 	if err := tlb.LoadFromCell(&report, c.BeginParse()); err != nil {
+		lggr.Errorw("OGT commitcodec.Decode tlb.LoadFromCell FAILED", "err", err)
 		return cciptypes.CommitPluginReport{}, fmt.Errorf("cannot decode commit report from cell: %w", err)
 	}
+
+	lggr.Debugw("OGT commitcodec.Decode tlb.LoadFromCell succeeded",
+		"numTokenPriceUpdates", len(report.PriceUpdates.TokenPriceUpdates),
+		"numGasPriceUpdates", len(report.PriceUpdates.GasPriceUpdates),
+		"numMerkleRoots", len(report.MerkleRoots))
 
 	priceUpdate := report.PriceUpdates
 	var tpuSlice []cciptypes.TokenPrice
@@ -138,6 +196,12 @@ func (cr *commitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (ccipty
 	if len(priceUpdate.GasPriceUpdates) > 0 {
 		gpuSlice = make([]cciptypes.GasPriceChain, len(priceUpdate.GasPriceUpdates))
 		for i, update := range priceUpdate.GasPriceUpdates {
+			lggr.Debugw("OGT commitcodec.Decode processing gas price update from TLB",
+				"index", i,
+				"chainSel", update.DestChainSelector,
+				"ExecutionGasPrice_field", update.ExecutionGasPrice.String(),
+				"DataAvailabilityGasPrice_field", update.DataAvailabilityGasPrice.String())
+
 			// Pack the two 112-bit fields back into a single 224-bit value
 			// Packed format: (DA << 112) | Exec
 			var packedPrice *big.Int
@@ -151,9 +215,21 @@ func (cr *commitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (ccipty
 				if daFee == nil {
 					daFee = big.NewInt(0)
 				}
+
+				lggr.Debugw("OGT commitcodec.Decode packing gas price",
+					"index", i,
+					"execFee", execFee.String(),
+					"daFee", daFee.String())
+
 				packedPrice = feequoter.PackGasPrice(execFee, daFee)
+
+				lggr.Debugw("OGT commitcodec.Decode packed gas price",
+					"index", i,
+					"packedPrice", packedPrice.String())
 			} else {
 				packedPrice = big.NewInt(0)
+				lggr.Debugw("OGT commitcodec.Decode gas price is zero",
+					"index", i)
 			}
 
 			gpuSlice[i] = cciptypes.GasPriceChain{
@@ -177,7 +253,7 @@ func (cr *commitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (ccipty
 		}
 	}
 
-	return cciptypes.CommitPluginReport{
+	result := cciptypes.CommitPluginReport{
 		PriceUpdates: cciptypes.PriceUpdates{
 			TokenPriceUpdates: tpuSlice,
 			GasPriceUpdates:   gpuSlice,
@@ -185,5 +261,12 @@ func (cr *commitPluginCodecV1) Decode(ctx context.Context, bytes []byte) (ccipty
 		BlessedMerkleRoots:   nil,
 		UnblessedMerkleRoots: merkleRoots,
 		RMNSignatures:        nil,
-	}, nil
+	}
+
+	lggr.Debugw("OGT commitcodec.Decode completed",
+		"numTokenPriceUpdates", len(result.PriceUpdates.TokenPriceUpdates),
+		"numGasPriceUpdates", len(result.PriceUpdates.GasPriceUpdates),
+		"numMerkleRoots", len(result.UnblessedMerkleRoots))
+
+	return result, nil
 }
