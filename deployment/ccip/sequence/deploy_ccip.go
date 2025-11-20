d10d05bc6370791e015619f6fef66872544fec16
Joe Huang <joe.huang@smartcontract.com>
2025-11-20 12:30:04 -0600

    rm unused

--- deployment/ccip/sequence/deploy_ccip.go
+++ deployment/ccip/sequence/deploy_ccip.go
@@ -4,7 +4,6 @@ import (
 	"math/big"
 
 	"github.com/xssnick/tonutils-go/address"
-	"github.com/xssnick/tonutils-go/tvm/cell"
 
 	mcmsOps "github.com/smartcontractkit/chainlink-ton/deployment/mcms/operation"
 	"github.com/smartcontractkit/chainlink-ton/deployment/utils"
