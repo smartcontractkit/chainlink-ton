6d78f58ee7533ab6ffdf10bf5397c769391ddb4c
Blaž Hrastnik <blaz@mxxn.io>
2025-11-28 17:54:34 +0900

    Update sha

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-inD80yI8xAQOn0xZmTZtCvwMMcaUozBBc4AYpwnX2g8=";
+  contracts = "sha256-7x/8Dgbij25cHeUpJhNw3APf04nRTJHSSmYWBnJZyXQ=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
