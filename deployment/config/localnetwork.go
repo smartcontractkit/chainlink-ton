404f9709daa1389d36af9b1d9d905ca373480775
Jade Park <jadepark.dev@gmail.com>
2025-12-03 17:12:11 +0000

    chore: testing default version

--- deployment/config/localnetwork.go
+++ deployment/config/localnetwork.go
@@ -25,7 +25,7 @@ func LocalNetworkConfig(once *sync.Once) cldf_provider.CTFChainProviderConfig {
 		// Source: https://github.com/neodix42/mylocalton-docker
 		Image: "ghcr.io/neodix42/mylocalton-docker:v3.96",
 		CustomEnv: map[string]string{
-			"VERSION_CAPABILITIES":        "12",  // This controls which TVM features are enabled in the local network.
+			// "VERSION_CAPABILITIES":        "12",  // This controls which TVM features are enabled in the local network.
 			"NEXT_BLOCK_GENERATION_DELAY": "0.5", // This is the recommended block generation delay for faster testing.
 		},
 	}
