ce007b1d139ac047d24459ee86546922be411941
Joe Huang <joe.huang@smartcontract.com>
2025-12-11 23:50:37 -0500

    bump hash

--- cmd/chainlink-ton/lock.nix
+++ cmd/chainlink-ton/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton = "sha256-HshVsNh+GA3mPxbTJZuIeTSVnsjk3a3LJ6ZIfk2Raf8=";
+  chainlink-ton = "sha256-u7RFGeXP9xS6otEnE8/Np2K7/dY6X/hsFJI1nEz30Uo=";
 }
