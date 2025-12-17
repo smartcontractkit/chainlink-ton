ee219666acb12873ca5a5c5484879d5d20dfb975
Joe Huang <joe.huang@smartcontract.com>
2025-12-17 12:21:38 -0600

    revert adaptor

--- deployment/ccip/1_6_0/sequences/adapter.go
+++ deployment/ccip/1_6_0/sequences/adapter.go
@@ -1,10 +1,19 @@
 package sequences
 
 import (
+	"fmt"
+
 	"github.com/Masterminds/semver/v3"
 	chainsel "github.com/smartcontractkit/chain-selectors"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	"github.com/xssnick/tonutils-go/address"
+
+	tonstate "github.com/smartcontractkit/chainlink-ton/deployment/state"
+
 	"github.com/smartcontractkit/chainlink-ccip/deployment/deploy"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/lanes"
+
+	"github.com/smartcontractkit/chainlink-ton/pkg/ccip/codec"
 )
 
 func init() {
@@ -17,3 +26,49 @@ func init() {
 }
 
 type TonAdapter struct{}
+
+func (a *TonAdapter) GetOnRampAddress(ds datastore.DataStore, chainSelector uint64) ([]byte, error) {
+	tonChain, err := tonstate.LoadOnchainStateUsingDataStore(ds, chainSelector)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to load TON onchain state: %w", err)
+	}
+
+	return convertAddress(tonChain.OnRamp)
+}
+
+func (a *TonAdapter) GetOffRampAddress(ds datastore.DataStore, chainSelector uint64) ([]byte, error) {
+	tonChain, err := tonstate.LoadOnchainStateUsingDataStore(ds, chainSelector)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to load TON onchain state: %w", err)
+	}
+
+	return convertAddress(tonChain.OffRamp)
+}
+
+func (a *TonAdapter) GetFQAddress(ds datastore.DataStore, chainSelector uint64) ([]byte, error) {
+	tonChain, err := tonstate.LoadOnchainStateUsingDataStore(ds, chainSelector)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to load TON onchain state: %w", err)
+	}
+
+	return convertAddress(tonChain.FeeQuoter)
+}
+
+func (a *TonAdapter) GetRouterAddress(ds datastore.DataStore, chainSelector uint64) ([]byte, error) {
+	tonChain, err := tonstate.LoadOnchainStateUsingDataStore(ds, chainSelector)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to load TON onchain state: %w", err)
+	}
+
+	return convertAddress(tonChain.Router)
+}
+
+func convertAddress(address address.Address) ([]byte, error) {
+	addrCodec := codec.NewAddressCodec()
+	rawAddress, err := addrCodec.AddressStringToBytes(address.String())
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to convert TON address to bytes: %w", err)
+	}
+
+	return rawAddress, nil
+}
