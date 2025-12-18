7dfe29dfb90952daedb2342e40024cb3c791dc4c
vicente <vicentevieytes@hotmail.com>
2025-12-18 17:06:23 -0300

    update ecMax

--- pkg/ccip/bindings/offramp/offramp.go
+++ pkg/ccip/bindings/offramp/offramp.go
@@ -349,7 +349,7 @@ var ExitCodeCodec tvm.ExitCodeCodecInt[ExitCode] = ExitCode(tvm.ExitCode(-1))
 func (ExitCode) NewFrom(ec tvm.ExitCode) (ExitCode, error) {
 	const (
 		ecMin = int32(ErrorMessageNotFromOwnedContract)
-		ecMax = int32(ErrorZeroAddressNotAllowed)
+		ecMax = int32(ErrorInvalidInterval)
 	)
 	return tvm.NewExitCodeInRange(ExitCode(ec), ecMin, ecMax)
 }
