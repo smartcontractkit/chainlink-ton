b9e9dea2799e383a7f7bdbb7712a70c180a3d957
Oliver Townsend <oliver.townsend@smartcontract.com>
2025-12-17 18:19:30 -0800

    fix logic and add doc

--- pkg/txm/txm.go
+++ pkg/txm/txm.go
@@ -233,7 +233,8 @@ func (t *Txm) broadcastLoop() {
 	}
 }
 
-// Attempts to broadcast a transaction with retries on failure.
+// broadcastWithRetry Attempts to broadcast a transaction with retries on failure. We only retry if there was a
+// failure to send the transaction, not if the transaction was broadcast but failed to execute (non-zero exit code).
 func (t *Txm) broadcastWithRetry(ctx context.Context, tx *Tx, msg *wallet.Message, txID string) error {
 	var receivedMessage *tracetracking.ReceivedMessage
 	var err error
@@ -244,7 +245,7 @@ func (t *Txm) broadcastWithRetry(ctx context.Context, tx *Tx, msg *wallet.Messag
 		return fmt.Errorf("failed to get client: %w", cerr)
 	}
 
-	// try to send transaction
+	// try to broadcast transaction
 	for attempt := uint(1); attempt <= t.config.MaxSendRetryAttempts; attempt++ {
 		t.logger.Debugw("sending transaction to TON",
 			"txID", txID,
@@ -255,19 +256,24 @@ func (t *Txm) broadcastWithRetry(ctx context.Context, tx *Tx, msg *wallet.Messag
 			"hasBody", msg.InternalMessage.Body != nil)
 		receivedMessage, _, err = client.SendWaitTransaction(ctx, tx.To, msg)
 		if err != nil {
-			t.logger.Errorw("SendWaitTransaction failed. Continuing with remaining attempts", "err", err, "txID", txID, "attempt", attempt)
-			continue
-		}
-
-		if receivedMessage.ExitCode != 0 {
-			t.logger.Errorw("transaction failed", "exitcode", receivedMessage.ExitCode, "description", receivedMessage.ExitCode.Describe())
-		}
-
-		if err == nil {
+			// Transaction failed to broadcast. Log error as a warning for now and fall through to retry delay below.
+			t.logger.Warnw("failed to broadcast tx, will retry",
+				"txID", txID,
+				"attempt", attempt,
+				"to", tx.To.String(),
+				"err", err)
+		} else {
 			t.logger.Infow("transaction broadcasted",
 				"txID", txID,
 				"to", tx.To.String(),
 				"amount", tx.Amount.Nano().String())
+
+			// Transaction was broadcast successfully, but ultimately failed to execute due to ExitCode.
+			if receivedMessage.ExitCode != 0 {
+				t.logger.Errorw("transaction failed", "exitcode", receivedMessage.ExitCode, "description", receivedMessage.ExitCode.Describe())
+			}
+
+			// Wait for and gather full trace regardless of exit code
 			err = receivedMessage.WaitForTrace(ctx, client.Client)
 			if err != nil {
 				t.logger.Errorw("failed to wait for trace", "error", err)
@@ -276,12 +282,6 @@ func (t *Txm) broadcastWithRetry(ctx context.Context, tx *Tx, msg *wallet.Messag
 			t.logger.Debugf("Msg sequence diagram:\n%s\n", debug.NewDebuggerSequenceTrace(nil, sequenceDiagram.OutputFmtURL).DumpReceived(receivedMessage))
 			break
 		}
-
-		t.logger.Warnw("failed to broadcast tx, will retry",
-			"txID", txID,
-			"attempt", attempt,
-			"to", tx.To.String(),
-			"err", err)
 
 		select {
 		case <-time.After(t.config.SendRetryDelay.Duration()):
