d6f57a9e102d8d0024956b211141b36e060a1877
Kristijan <kristijan.rebernisak@gmail.com>
2025-12-22 18:47:18 +0100

    Add SendMessageOp and AnySequence

---
+++ deployment/pkg/ops/ops.go
@@ -0,0 +1,115 @@
+package ops // alias: opston
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	"github.com/smartcontractkit/chainlink-ton/pkg/ton/debug/lib"
+
+	"github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/ton/wallet"
+)
+
+var (
+	_ MessagePlannerOption = SendMessageInput[any]{}
+	_ MessagePlanner       = SendMessageOutput{}
+	_ MessageSender        = SendMessageOutput{}
+)
+
+type SendMessageInput[T any] struct {
+	Envelope lib.MessageEnvelope[T] `json:"envelope"`
+	Plan     bool                   `json:"plan"`
+
+	// Tx options
+	DstAddr *address.Address `json:"dst_addr"`
+	Amount  tlb.Coins        `json:"amount"`
+}
+
+func (in SendMessageInput[T]) IsPlan() bool {
+	return in.Plan
+}
+
+type SendMessageOutput struct {
+	Plan        MessagePlanRaw   `json:"plan"`
+	Transaction *TransactionInfo `json:"transaction,omitempty"`
+}
+
+func (o SendMessageOutput) GetPlan() MessagePlanRaw {
+	return o.Plan
+}
+
+func (o SendMessageOutput) GetTransaction() *TransactionInfo {
+	return o.Transaction
+}
+
+type SendMessageDeps struct {
+	Wallet *wallet.Wallet
+}
+
+func handler[T any](b operations.Bundle, deps SendMessageDeps, in SendMessageInput[T]) (SendMessageOutput, error) {
+	ctx := b.GetContext()
+
+	body, err := tlb.ToCell(*in.Envelope.Value)
+	if err != nil {
+		return SendMessageOutput{}, fmt.Errorf("failed to convert message to cell: %w", err)
+	}
+
+	plan := MessagePlanRaw{
+		Body:    body,
+		DstAddr: in.DstAddr,
+		Amount:  in.Amount,
+	}
+
+	if in.Plan {
+		return SendMessageOutput{
+			Plan:        plan,
+			Transaction: nil,
+		}, nil
+	}
+
+	msg := &wallet.Message{
+		Mode: wallet.PayGasSeparately | wallet.IgnoreErrors,
+		InternalMessage: &tlb.InternalMessage{
+			IHRDisabled: true,
+			Bounce:      true,
+			DstAddr:     in.DstAddr,
+			Amount:      in.Amount,
+			Body:        body,
+		},
+	}
+
+	tx, _, err := deps.Wallet.SendWaitTransaction(ctx, msg)
+	if err != nil {
+		return SendMessageOutput{}, fmt.Errorf("failed to send transaction: %w", err)
+	}
+
+	return SendMessageOutput{
+		Plan: plan,
+		Transaction: &TransactionInfo{
+			// TODO: AccountAddr
+			Hash:        hex.EncodeToString(tx.Hash),
+			OutMsgCount: tx.OutMsgCount,
+			EndStatus:   tx.EndStatus,
+			TotalFees:   tx.TotalFees.Coins,
+		},
+	}, nil
+}
+
+// TODO: do we need this? Same as operations.Definition
+type OpOpts struct {
+	Version *semver.Version
+	Name    string
+	Desc    string
+}
+
+// TODO: make an instance (vs. factory) similar to AnySequence sequence
+func NewSendMessageOp[T any](opts OpOpts) *operations.Operation[SendMessageInput[T], SendMessageOutput, SendMessageDeps] {
+	return operations.NewOperation(opts.Name, opts.Version, opts.Desc, handler[T])
+}
+
+// TODO: add SendManyMessagesOp (check ExecuteTransactions in deployment/ccip/helpers/execute.go)
+// TODO: add WaitTrace option
---
+++ deployment/pkg/ops/sequence.go
@@ -0,0 +1,97 @@
+package ops // alias: opston
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+)
+
+type AnySequenceInput struct {
+	// Definitions and Inputs should be of the same length and order
+	Defs   []operations.Definition
+	Inputs []any // Each element should be the corresponding input type for its operation
+
+	ChainSelector uint64
+}
+
+// TODO: reuse MessagePlannerOption, MessagePlanner, MessageSender interfaces for
+// sequences as well. The interfaces would need to return a collection of plans/txs,
+// where an operation or a sequence may plan/produce multiple messages.
+type AnySequenceOutput struct {
+	Plans        []MessagePlanRaw
+	Transactions []TransactionInfo
+	// TODO: add deployed contract addresses mapping
+}
+
+// AnySequenceDeps is a generic map of dependencies
+// for ops/seq that may require different types
+type AnySequenceDeps map[string]any
+
+var AnySequence = operations.NewSequence(
+	"ton/sequence/any",
+	semver.MustParse("0.1.0"),
+	"Executes and/or plans a sequence of operations as defined by the inputs",
+	anySequenceHandler,
+)
+
+func anySequenceHandler(b operations.Bundle, deps AnySequenceDeps, in AnySequenceInput) (AnySequenceOutput, error) {
+	// Initialize the output
+	output := AnySequenceOutput{
+		Plans:        make([]MessagePlanRaw, 0),
+		Transactions: make([]TransactionInfo, 0),
+	}
+
+	for i, def := range in.Defs {
+		op, err := b.OperationRegistry.Retrieve(def)
+		if err != nil {
+			return output, fmt.Errorf("failed to retrieve operation %s: %w", def.ID, err)
+		}
+
+		// TODO: source requirements and inject deps for each op
+		key := def.ID
+		opdeps, ok := deps[key]
+		if !ok {
+			return output, fmt.Errorf("missing dependencies for operation %s", def.ID)
+		}
+
+		r, err := operations.ExecuteOperation(b, op, opdeps, in.Inputs[i])
+		if err != nil {
+			return output, fmt.Errorf("failed to execute operation %s: %w", def.ID, err)
+		}
+
+		// Extract plan and transaction info from the output
+		po, ok := r.Input.(MessagePlannerOption)
+		if ok && po.IsPlan() {
+			// If planning option is set, extract the plan
+			planer, ok := r.Output.(MessagePlanner)
+			if !ok {
+				return output, fmt.Errorf("operation %s output does not implement MessagePlanner interface", def.ID)
+			}
+			output.Plans = append(output.Plans, planer.GetPlan())
+		}
+
+		sender, ok := r.Output.(MessageSender)
+		if ok {
+			tx := sender.GetTransaction()
+			if tx != nil {
+				po, ok := r.Input.(MessagePlannerOption)
+				if ok && po.IsPlan() {
+					return output, fmt.Errorf("operation %s declared as a plan but returned a transaction", def.ID)
+				}
+				output.Transactions = append(output.Transactions, *tx)
+			}
+		}
+	}
+
+	return output, nil
+}
+
+// TODO: figure out how to best implement MCMS/Timelock wrapping sequence/changeset
+//
+// var MCMSTimelockAnySequence = operations.NewSequence(
+// 	"ton/sequence/mcms/any-via-timelock",
+// 	semver.MustParse("0.1.0"),
+// 	"Executes and/or plans (via MCMS/Timelock) a sequence of operations as defined by the inputs",
+// 	anySequenceHandler2,
+// )
