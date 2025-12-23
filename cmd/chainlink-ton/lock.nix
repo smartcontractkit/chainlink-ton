5ee418a86b665dc996a8df4d0f181218e14fb013
Joe Huang <joe.huang@smartcontract.com>
2025-12-23 14:04:45 -0600

    fix nix again

--- cmd/chainlink-ton/lock.nix
+++ cmd/chainlink-ton/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton = "sha256-/wlH3XHV8wwe2Sjatg6YkkcD1N7zNuwyMvFJZ/4V98M=";
+  chainlink-ton = "sha256-N3PTCZTFMrPRFnBhoi3qERAVhe7uicRAKRMg7w9Rce4=";
 }
