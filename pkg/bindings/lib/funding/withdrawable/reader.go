9b5315d85f613c09d882c65b9da98508fe22efb4
Kristijan <kristijan.rebernisak@gmail.com>
2025-12-30 10:41:05 +0100

    Add withdrawable Go bindings - NONEVM-3144

---
+++ pkg/bindings/lib/funding/withdrawable/reader.go
@@ -0,0 +1,22 @@
+package withdrawable
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/xssnick/tonutils-go/ton"
+
+	"github.com/smartcontractkit/chainlink-ton/pkg/ton/tvm"
+)
+
+var GetReserve = tvm.NewNoArgsGetter(tvm.NoArgsOpts[*big.Int]{
+	Name: "reserve",
+	Decoder: tvm.NewResultDecoder(func(r *ton.ExecutionResult) (*big.Int, error) {
+		ri, err := r.Int(0)
+		if err != nil {
+			return nil, fmt.Errorf("error getting Int(0) - reserve: %w", err)
+		}
+
+		return ri, nil
+	}),
+})
