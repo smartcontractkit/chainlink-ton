31dbfb17ccd724a065dd69c82e5e4ca7a9f37cb1
Kristijan <kristijan.rebernisak@gmail.com>
2026-01-01 13:07:31 +0100

    Add more resolvers (msg-envelope-to-cell, contract-meta-to-cell)

---
+++ deployment/pkg/codec/resolvers/contract_meta_to_cell.go
@@ -0,0 +1,53 @@
+package resolvers // alias: resolversd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/xssnick/tonutils-go/tvm/cell"
+
+	"github.com/smartcontractkit/chainlink-ton/pkg/ton/codec"
+
+	"github.com/smartcontractkit/chainlink-ton/deployment/pkg/ops/ton"
+)
+
+var (
+	_ codec.Resolver[map[string]any, *cell.Cell] = (*contractToCellResolver)(nil)
+	_ codec.ResolverKeyProvider                  = (*contractToCellResolver)(nil)
+)
+
+type contractToCellResolver struct {
+	provider ton.ContractProvider
+}
+
+func NewContractToCellResolver(provider ton.ContractProvider) codec.Resolver[map[string]any, *cell.Cell] {
+	return &contractToCellResolver{provider: provider}
+}
+
+func (r contractToCellResolver) Key() string {
+	return "codec.resolvers.contract-meta-to-cell"
+}
+
+func (r contractToCellResolver) Resolve(input map[string]any) (*cell.Cell, error) {
+	data, ok := input["data"]
+	if !ok {
+		return nil, fmt.Errorf("missing 'data' field in input: %v", input)
+	}
+
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal 'data' field: %w", err)
+	}
+
+	var meta ton.ContractMetadata
+	err = json.Unmarshal(dataBytes, &meta)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal 'data' field to MessageEnvelope: %w", err)
+	}
+
+	contract, err := r.provider.GetContract(meta)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get contract: %w", err)
+	}
+	return contract.Code, nil
+}
