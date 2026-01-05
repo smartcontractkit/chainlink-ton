e025d5df98b05d7e1c1f03c029d5c1d416ecd326
Joe Huang <joe.huang@smartcontract.com>
2026-01-05 15:22:21 -0600

    fix

--- scripts/oplint/lock.nix
+++ scripts/oplint/lock.nix
@@ -1,7 +1,3 @@
 {pkgs}: {
-<<<<<<< NONEVM-3219/CCIP-deployer-interface
-  oplint = "sha256-YKlMMOliCW7rm03Jdn5Trv2hZBdsNJI8LrXQPFhU28M=";
-=======
   oplint = "sha256-eJfe7EaeYj/q/s/Ph68sc6XDv4VT5MTqMRsYbD026/w=";
->>>>>>> main
 }
