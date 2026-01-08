f55956c50b4e5035c5bcd33b8120339c1ef2ff43
Terry Tata <terry.tata@smartcontract.com>
2026-01-08 06:46:42 -0800

    lint

--- deployment/utils/chain.go
+++ deployment/utils/chain.go
@@ -43,7 +43,7 @@ func FundWallets(t *testing.T, client ton.APIClientWrapped, recipients []*addres
 }
 
 func FundWalletsNoT(client ton.APIClientWrapped, recipients []*address.Address, amounts []tlb.Coins) error {
-    return FundWalletsWithCtx(context.Background(), client, recipients, amounts)
+	return FundWalletsWithCtx(context.Background(), client, recipients, amounts)
 }
 
 func FundWalletsWithCtx(ctx context.Context, client ton.APIClientWrapped, recipients []*address.Address, amounts []tlb.Coins) error {
