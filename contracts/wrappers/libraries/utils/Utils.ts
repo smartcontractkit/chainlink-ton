3c3afd63e2c72a5a25d0429cf32dae48043ff32b
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-01-08 16:26:56 -0300

    ref: align opcodes in Utils

---
+++ contracts/wrappers/libraries/utils/Utils.ts
@@ -0,0 +1,8 @@
+export const FACILITY_NAME = 'com.chainlink.ton.lib.Utils'
+export const FACILITY_ID = 489
+export const ERROR_CODE = FACILITY_ID * 100
+
+export enum Errors {
+  InvalidData = ERROR_CODE,
+  BitmapOutOfBounds,
+}
