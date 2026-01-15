46f99385c231891043ca67d716bff37a798bfda5
Jade Park <jadepark.dev@gmail.com>
2026-01-15 22:04:51 +0000

    chore: lock hashes

--- cmd/chainlink-ton/lock.nix
+++ cmd/chainlink-ton/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton = "sha256-2d+xZPLdHWBQYutoHEuQXkaaQlzJYMJhkxooWBiy14U=";
+  chainlink-ton = "sha256-0iQ+dqQvXHCcBYr8Yl0LjhK67yfgJUFUdunuSgZC8A8=";
 }
--- scripts/oplint/lock.nix
+++ scripts/oplint/lock.nix
@@ -1,3 +1,3 @@
 {pkgs}: {
-  oplint = "sha256-2d+xZPLdHWBQYutoHEuQXkaaQlzJYMJhkxooWBiy14U=";
+  oplint = "sha256-0iQ+dqQvXHCcBYr8Yl0LjhK67yfgJUFUdunuSgZC8A8=";
 }
