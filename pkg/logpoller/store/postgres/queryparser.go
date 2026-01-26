669aeddfd4d9ec2267b1b5a57859ab4484023aeb
Jade Park <jadepark.dev@gmail.com>
2026-01-26 21:02:07 +0000

    fix: omit db specific field from select

--- pkg/logpoller/store/postgres/queryparser.go
+++ pkg/logpoller/store/postgres/queryparser.go
@@ -40,25 +40,22 @@ func newQueryParser(chainID string) *queryParser {
 	// This is required because multiple filters can track the same log events.
 	// Deduplication is also done in-memory after query for additional safety.
 	builder.query.WriteString(`SELECT DISTINCT
-		id, 
-		filter_id, 
-		chain_id, 
-		address, 
-		event_sig, 
+		chain_id,
+		address,
+		event_sig,
 		data_header,
 		data_payload,
-		tx_hash, 
-		tx_lt, 
-		msg_index, 
-		tx_timestamp, 
-		block_workchain, 
-		block_shard, 
-		block_seqno, 
-		block_root_hash, 
-		block_file_hash, 
-		master_block_seqno, 
-		msg_lt,
-		created_at
+		tx_hash,
+		tx_lt,
+		msg_index,
+		tx_timestamp,
+		block_workchain,
+		block_shard,
+		block_seqno,
+		block_root_hash,
+		block_file_hash,
+		master_block_seqno,
+		msg_lt
 	FROM ton.log_poller_logs`)
 	return builder
 }
