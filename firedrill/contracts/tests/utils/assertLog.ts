758c344eb39994074a1c1d2add619c36758496fb
vicente <vicentevieytes@hotmail.com>
2026-01-28 12:02:28 -0300

    define test suites for firedrill contracts

---
+++ firedrill/contracts/tests/utils/assertLog.ts
@@ -0,0 +1,3 @@
+// Re-export assertLog from the main contracts to ensure type compatibility
+export { assertLog } from '../../../../contracts/tests/Logs'
+export { LogTypes } from '../../../../contracts/wrappers/ccip/Logs'
