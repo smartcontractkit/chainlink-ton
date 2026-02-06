a439180c45549ab1684b5db809b6908cc84b60dc
Jade Park <jadepark.dev@gmail.com>
2026-02-06 13:07:14 +0000

    chore: clean up

--- devenv/test_framework.go
+++ devenv/test_framework.go
@@ -3,13 +3,8 @@ package ccipton
 import (
 	"context"
 	"errors"
-	"math/big"
 )
 
 func (m *CCIP16TON) GetEOAReceiverAddress(ctx context.Context, chainSelector uint64) ([]byte, error) {
 	return nil, errors.New("GetEOAReceiverAddress not implemented for TON")
-}
-
-func (m *CCIP16TON) GetTokenBalance(ctx context.Context, chainSelector uint64, address, tokenAddress []byte) (*big.Int, error) {
-	return nil, errors.New("GetTokenBalance not implemented for TON")
 }
