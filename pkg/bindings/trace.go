8573bcdd18350a4ae168c3e976e6b537d91fd9f1
Kristijan <kristijan.rebernisak@gmail.com>
2026-02-09 13:07:33 +0100

    Polish DefaultTraceStopCondition policy

--- pkg/bindings/trace.go
+++ pkg/bindings/trace.go
@@ -3,6 +3,7 @@ package bindings
 import (
 	"fmt"
 
+	"github.com/smartcontractkit/chainlink-ton/pkg/bindings/lib/access/rbac"
 	"github.com/smartcontractkit/chainlink-ton/pkg/ccip/bindings/offramp"
 	"github.com/smartcontractkit/chainlink-ton/pkg/ccip/bindings/router"
 	"github.com/smartcontractkit/chainlink-ton/pkg/ton/tracetracking"
@@ -14,9 +15,6 @@ import (
 //
 // Notice: we expect account contracts (e.g., Wallet contracts, MCMS/Timelock, multisigs, etc) to accept
 // all incoming messages (replies) and not reject with 0xffff (wrong opcode) but accept/ignore.
-//
-//   - we don't track fwd notifications to uninitialized accounts, bounce = false
-//   - we don't track outgoing msgs/notifications from third-party CCIP receiver contracts (except for router.CCIPReceiveConfirm)
 var DefaultTraceStopCondition tracetracking.StopCondition = func(parent, current *tracetracking.ReceivedMessage) (bool, error) {
 	// Check if internal messages exist, or continue (e.g., for external messages)
 	if parent.InternalMsg == nil || current.InternalMsg == nil {
@@ -28,12 +26,7 @@ var DefaultTraceStopCondition tracetracking.StopCondition = func(parent, current
 		return false, fmt.Errorf("failed to get exit code: %w", err)
 	}
 
-	// Stop tracing on NoState exit code (fwd notifications to uninitialized accounts, bounce = false)
-	if !current.InternalMsg.Bounce && ec == tvm.ExitCodeComputeSkipReasonNoState {
-		return true, nil // stop tracing
-	}
-
-	// Check specific opcodes for MCMS/CCIP messages to determine trace boundaries
+	// Check specific conditions and opcodes for MCMS/CCIP messages to determine trace boundaries
 	opcodeParent, err := tvm.ExtractOpcode(parent.InternalMsg.Body)
 	if err != nil {
 		return false, fmt.Errorf("failed to extract opcode from parent message: %w", err)
@@ -44,7 +37,20 @@ var DefaultTraceStopCondition tracetracking.StopCondition = func(parent, current
 		return false, fmt.Errorf("failed to extract opcode from current message: %w", err)
 	}
 
+	// Stop tracing on NoState exit code (fwd notifications to uninitialized accounts)
+	if ec == tvm.ExitCodeComputeSkipReasonNoState {
+		switch uint64(opcodeCurrent) {
+		case 0:
+			return true, nil // allow empty messages to uninitialized accounts (stop tracing)
+		case rbac.OpcodeRoleGranted:
+			return true, nil // allow ac forward notifications to uninitialized accounts (stop tracing)
+		case rbac.OpcodeRoleRevoked:
+			return true, nil // allow ac forward notifications to uninitialized accounts (stop tracing)
+		}
+	}
+
 	switch uint64(opcodeParent) {
+	// Trace the CCIPReceive flow, exclude the receiver downstream messages
 	case offramp.OpcodeCCIPReceive:
 		// Stop tracing if the current message is not router.CCIPReceiveConfirm (i.e. don't consider
 		// any other outgoing msgs/notifications from third-party CCIP receiver contracts - on offramp.CCIPReceive)
