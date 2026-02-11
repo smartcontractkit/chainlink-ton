eb90f3dc7f14f9dac090001a6cb01c161996c60e
Joe Huang <joe.huang@smartcontract.com>
2026-02-10 21:10:08 -0600

    update

--- pkg/ccip/bindings/ownable2step/storage.go
+++ pkg/ccip/bindings/ownable2step/storage.go
@@ -7,5 +7,5 @@ import (
 // Ownable2Step represents a two-step ownership structure, where an owner can set a pending owner.
 type Storage struct {
 	Owner        *address.Address `tlb:"addr"`
-	PendingOwner *address.Address `tlb:"addr"` // PendingOwner is optional
+	PendingOwner *address.Address `tlb:"addr"`
 }
