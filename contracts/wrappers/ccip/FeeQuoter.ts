abe7d4db34f1fdfc9dcaa4377f40b89592a72b3b
vicente <vicentevieytes@hotmail.com>
2026-02-26 16:16:01 -0300

    bump feequoter version and fix migrate function

--- contracts/wrappers/ccip/FeeQuoter.ts
+++ contracts/wrappers/ccip/FeeQuoter.ts
@@ -29,7 +29,7 @@ import * as upgradeable from '../libraries/versioning/Upgradeable'
 import * as typeAndVersion from '../libraries/versioning/TypeAndVersion'
 import * as rt from './Router'
 
-export const FEE_QUOTER_CONTRACT_VERSION = '1.6.0'
+export const FEE_QUOTER_CONTRACT_VERSION = '1.6.1'
 
 export const FACILITY_NAME = 'link.chain.ton.ccip.FeeQuoter'
 export const FACILITY_ID = facilityId(crc32(FACILITY_NAME))
