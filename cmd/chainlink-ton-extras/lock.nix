4bf2106c72bf43bb72b9253df5ab55b3a982d3e8
Terry Tata <terry.tata@smartcontract.com>
2026-03-17 11:52:47 -0700

    bump again

--- cmd/chainlink-ton-extras/lock.nix
+++ cmd/chainlink-ton-extras/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton-extras = "sha256-U2ripSxRWGBjAb18sd17500VqGI8C09CQBPp7yBkx30=";
+  chainlink-ton-extras = "sha256-R04fqH8gerrTZyfXUHa1qrtYQdyTA5d1ZO+fbF1TbGs=";
 }
