1dc7ed6672d3c3a2c2f8ac2a8764b3b09f883b6c
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-03-18 10:22:04 -0300

    fix: scripts

--- contracts/scripts/getKeyPair.ts
+++ contracts/scripts/getKeyPair.ts
@@ -12,16 +12,32 @@ run(args)
   })
 
 export async function run(args: string[]) {
-  if (args.length != 1 && args.length != 12 && args.length != 24) {
-    throw new Error('Usage: yarn run getKeyPair <mnemonic>')
+  if (args.length < 1) {
+    throw new Error('Usage: yarn run getKeyPair --testnet|--mainnet <mnemonic>')
+  }
+  const chain = args[0]
+  if (chain !== '--testnet' && chain !== '--mainnet') {
+    throw new Error('Usage: yarn run getKeyPair --testnet|--mainnet <mnemonic>')
+  }
+  const mnemonic = args.slice(1)
+  if (mnemonic.length != 1 && mnemonic.length != 12 && mnemonic.length != 24) {
+    throw new Error('Usage: yarn run getKeyPair --testnet|--mainnet <mnemonic>')
   }
 
-  const mnemonicArray = args.length == 1 ? args[0].split(' ') : args
+  const mnemonicArray = mnemonic.length == 1 ? mnemonic[0].split(' ') : mnemonic
 
   // derive private and public keys from the mnemonic
   const keyPair = await mnemonicToPrivateKey(mnemonicArray)
   const walletKey = await mnemonicToWalletKey(mnemonicArray)
-  const wallet = WalletContractV5R1.create({ workchain: 0, publicKey: walletKey.publicKey })
+  const wallet = WalletContractV5R1.create({
+    workchain: 0,
+    publicKey: walletKey.publicKey,
+    walletId: {
+      // -239 is mainnet, -3 is testnet
+      networkGlobalId: chain === '--mainnet' ? -239 : -3,
+      context: 0,
+    },
+  })
 
   console.log('Public Key: ' + keyPair.publicKey.toString('hex'))
   console.log('Private Key: ' + keyPair.secretKey.toString('hex'))
