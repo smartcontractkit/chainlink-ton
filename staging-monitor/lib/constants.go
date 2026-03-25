efedd37a68e5f3dd25de334d4dcc433aa3401aea
vicente <vicentevieytes@hotmail.com>
2026-03-25 20:09:33 -0300

    increase monitor deadline to 25mins

--- staging-monitor/lib/constants.go
+++ staging-monitor/lib/constants.go
@@ -4,7 +4,7 @@ import "time"
 
 // Test timeouts and intervals
 const (
-	TestTimeout         = 20 * time.Minute
+	TestTimeout         = 25 * time.Minute
 	PollInterval        = 4 * time.Second
 	ProgressLogInterval = 15 * time.Second
 )
