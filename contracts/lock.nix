915794264c257a77b2928b46eb3b0802d86e8dae
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-04-10 17:35:40 -0300

    fix: bump nix hash

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-TuW9wXjtxq3y0UajUNJXJBNy96m3DzSPDLzI8XATg8o=";
+  contracts = "sha256-tFC2ph7R21tS4I9kuqOXddxdZOPugBZaVHQ/1t/XpME=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
