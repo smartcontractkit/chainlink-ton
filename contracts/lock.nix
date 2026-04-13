7b8467e7c98ea41f4d106820c76f3dc0de94dae5
Kristijan <kristijan.rebernisak@gmail.com>
2026-04-13 15:19:49 +0200

    rm root @multiformats/murmur3

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-7wi34InQSz0JNbyLpS6cXHUCaMq9iTRWjsEgqvrjNIQ=";
+  contracts = "sha256-tFC2ph7R21tS4I9kuqOXddxdZOPugBZaVHQ/1t/XpME=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
