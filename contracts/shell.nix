2e7e1d66bf93b15ef868314c8386ebe66503ea74
Kristijan <kristijan.rebernisak@gmail.com>
2026-04-14 19:01:57 +0200

    Fetch CCIP 1.6.0 contract package from GH release (tag)

--- contracts/shell.nix
+++ contracts/shell.nix
@@ -2,6 +2,7 @@
   stdenv,
   pkgs,
   lib,
+  contracts_1_6,
   jetton-contracts,
   oplint,
 }:
@@ -25,8 +26,11 @@ pkgs.mkShell {
   ];
 
   PATH_CONTRACTS_JETTON = "${jetton-contracts}/lib/node_modules/jetton/build/";
+  PATH_CONTRACTS_1_6 = "${contracts_1_6}"; # Notice: loaded from GH release, artifacts in root of the package
 
   shellHook = ''
-    echo "Jetton contracts located here: $PATH_CONTRACTS_JETTON"
+    echo "Loaded TVM contracts at following paths:"
+    echo "  - CCIP 1.6.0: (env:PATH_CONTRACTS_1_6)    $PATH_CONTRACTS_1_6"
+    echo "  - Jetton:     (env:PATH_CONTRACTS_JETTON) $PATH_CONTRACTS_JETTON"
   '';
 }
