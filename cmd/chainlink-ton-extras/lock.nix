94b8ad7ab0a65ec0370a9ca5667032d216f8eff2
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-05-19 15:25:04 -0300

    fix: bump nix

--- cmd/chainlink-ton-extras/lock.nix
+++ cmd/chainlink-ton-extras/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton-extras = "sha256-egm3TSMzfQkryP51w7+hkWUPDd/TUcloJf4Pp+2DQmQ=";
+  chainlink-ton-extras = "sha256-pjWmDf1X3VRUBOuBSWOzOuBcgHgkhYAasQKHOMCCD2k=";
 }
--- scripts/oplint/lock.nix
+++ scripts/oplint/lock.nix
@@ -1,3 +1,3 @@
 {pkgs}: {
-  oplint = "sha256-egm3TSMzfQkryP51w7+hkWUPDd/TUcloJf4Pp+2DQmQ=";
+  oplint = "sha256-pjWmDf1X3VRUBOuBSWOzOuBcgHgkhYAasQKHOMCCD2k=";
 }
