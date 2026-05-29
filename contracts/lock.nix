d2f94e60bfe28af4655c601c01a28c6fac0e19c7
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-05-29 17:17:26 -0300

    fix: old axios version vulnerability

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-hdfsb2DVaDWE3kFG8nTMTJrXOprkssjPM/nUzszRFmg=";
+  contracts = "sha256-3F9NejeWPeLm6idkWDe8eKHssOSocZxvq33DywwMi/U=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
