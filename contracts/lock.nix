1bb1b8f24cc79653ed1931f2d960a795b3544ad7
Kristijan <kristijan.rebernisak@gmail.com>
2026-06-03 10:46:26 +0200

    Add pnpm ovverrides - axios

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-3F9NejeWPeLm6idkWDe8eKHssOSocZxvq33DywwMi/U=";
+  contracts = "sha256-4xgoRTE5stQ6CSC76f3oW2qDG0sXDvDIfIKAYBoC1ZY=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
