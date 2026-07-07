b002660df6e7a4f7012d5b82564f8f36e39e4d97
vicente <vicentevieytes@hotmail.com>
2026-07-07 12:00:54 -0300

    uncomment code skipping jetton tests

--- integration-tests/jetton/jetton_test.go
+++ integration-tests/jetton/jetton_test.go
@@ -47,10 +47,10 @@ const JettonDataURI = "smartcontract.com"
 var jettonMintingAmount tlb.Coins = tlb.MustFromTON("100")
 
 func TestJettonAll(t *testing.T) {
-	//t.Skip("Skipping jetton tests for now to speed up CI")
-	//if testing.Short() {
-	//	t.Skip("Skipping long-running test in short mode")
-	//}
+	t.Skip("Skipping jetton tests for now to speed up CI")
+	if testing.Short() {
+		t.Skip("Skipping long-running test in short mode")
+	}
 	// Common test setup
 	type commonSetup struct {
 		deployer         tracetracking.SignedAPIClient
