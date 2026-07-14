0a3516e3ce73a5543de13c50c67c5921ff8cb90f
Patricio Tourne Passarino <patricio.passarino@smartcontract.com>
2026-07-14 12:09:29 -0300

    ref: extract test cases

--- contracts/wrappers/test/lib/math.ts
+++ contracts/wrappers/test/lib/math.ts
@@ -32,52 +32,72 @@ export class ContractClient implements Contract {
     await p.internal(via, { value, sendMode: SendMode.PAY_GAS_SEPARATELY, body })
   }
 
-  async getSafeProd(p: ContractProvider, a: bigint, b: bigint) {
-    return p.get('get_safeProd', [
+  async getSafeProd(
+    p: ContractProvider,
+    a: bigint,
+    b: bigint,
+  ): Promise<{ result: bigint; errorCode: bigint }> {
+    const r = await p.get('get_safeProd', [
       { type: 'int', value: a },
       { type: 'int', value: b },
     ])
+    return {
+      result: r.stack.readBigNumber(),
+      errorCode: r.stack.readBigNumber(),
+    }
   }
-  async getSafeAdd(p: ContractProvider, a: bigint, b: bigint) {
-    return p.get('get_safeAdd', [
+  async getSafeAdd(
+    p: ContractProvider,
+    a: bigint,
+    b: bigint,
+  ): Promise<{ result: bigint; errorCode: bigint }> {
+    const r = await p.get('get_safeAdd', [
       { type: 'int', value: a },
       { type: 'int', value: b },
     ])
+    return {
+      result: r.stack.readBigNumber(),
+      errorCode: r.stack.readBigNumber(),
+    }
   }
   async getSafePow10(
     p: ContractProvider,
     n: number,
   ): Promise<{ result: bigint; errorCode: bigint }> {
-    const result = await p.get('get_safePow10', [{ type: 'int', value: BigInt(n) }])
+    const r = await p.get('get_safePow10', [{ type: 'int', value: BigInt(n) }])
     return {
-      result: result.stack.readBigNumber(),
-      errorCode: result.stack.readBigNumber(),
+      result: r.stack.readBigNumber(),
+      errorCode: r.stack.readBigNumber(),
     }
   }
-  async getMustAdd(p: ContractProvider, a: bigint, b: bigint, errCode: bigint) {
-    return p.get('get_mustAdd', [
+  async getMustAdd(p: ContractProvider, a: bigint, b: bigint, errCode: bigint): Promise<bigint> {
+    const r = await p.get('get_mustAdd', [
       { type: 'int', value: a },
       { type: 'int', value: b },
       { type: 'int', value: errCode },
     ])
+    return r.stack.readBigNumber()
   }
-  async getMustProd(p: ContractProvider, a: bigint, b: bigint, errCode: bigint) {
-    return p.get('get_mustProd', [
+  async getMustProd(p: ContractProvider, a: bigint, b: bigint, errCode: bigint): Promise<bigint> {
+    const r = await p.get('get_mustProd', [
       { type: 'int', value: a },
       { type: 'int', value: b },
       { type: 'int', value: errCode },
     ])
+    return r.stack.readBigNumber()
   }
-  async getMustCastToCoin(p: ContractProvider, value: bigint, errCode: bigint) {
-    return p.get('get_mustCastToCoin', [
+  async getMustCastToCoin(p: ContractProvider, value: bigint, errCode: bigint): Promise<bigint> {
+    const r = await p.get('get_mustCastToCoin', [
       { type: 'int', value: value },
       { type: 'int', value: errCode },
     ])
+    return r.stack.readBigNumber()
   }
-  async getMustPow10(p: ContractProvider, n: number, errCode: bigint) {
-    return p.get('get_mustPow10', [
+  async getMustPow10(p: ContractProvider, n: number, errCode: bigint): Promise<bigint> {
+    const r = await p.get('get_mustPow10', [
       { type: 'int', value: BigInt(n) },
       { type: 'int', value: errCode },
     ])
+    return r.stack.readBigNumber()
   }
 }
