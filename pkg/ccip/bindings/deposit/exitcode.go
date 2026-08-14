cf792f0591b561e32e4c8148fde241de8ee750ce
Kristijan <kristijan.rebernisak@gmail.com>
2026-08-14 14:59:16 +0200

    DepositAccount Go bindings

---
+++ pkg/ccip/bindings/deposit/exitcode.go
@@ -0,0 +1,23 @@
+package deposit
+
+import "github.com/smartcontractkit/chainlink-ton/cciplib/ton/tvm"
+
+//go:generate go run golang.org/x/tools/cmd/stringer@v0.38.0 -type=ExitCode -trimprefix=ExitCode -output=exitcode_string.go
+type ExitCode tvm.ExitCode
+
+var ExitCodeCodec tvm.ExitCodeCodecInt[ExitCode] = ExitCode(tvm.ExitCode(-1))
+
+func (ExitCode) NewFrom(ec tvm.ExitCode) (ExitCode, error) {
+	const (
+		ecMin = int32(ExitCodeOnlyOwner)
+		ecMax = int32(ExitCodeOnlySendExcessesToSender)
+	)
+	return tvm.NewExitCodeInRange(ExitCode(ec), ecMin, ecMax)
+}
+
+const (
+	// Facility ID 80 * 100 = 8000
+	ExitCodeOnlyOwner ExitCode = iota + 8000
+	ExitCodeOnlyBeneficiary
+	ExitCodeOnlySendExcessesToSender
+)
---
+++ pkg/ccip/bindings/deposit/types.go
@@ -0,0 +1,98 @@
+package deposit
+
+import (
+	"github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/tvm/cell"
+
+	"github.com/smartcontractkit/chainlink-ton/cciplib/ccip/bindings/common"
+	"github.com/smartcontractkit/chainlink-ton/cciplib/ton/tlbe"
+	"github.com/smartcontractkit/chainlink-ton/cciplib/ton/tvm"
+	"github.com/smartcontractkit/chainlink-ton/pkg/bindings/jetton/wallet"
+)
+
+// --- Constants ---
+
+// Name and version reported by typeAndVersion() — matches deposit/types.tolk.
+const (
+	ContractName    = "link.chain.ton.ccip.DepositAccount"
+	ContractVersion = "0.1.0"
+)
+
+// --- Data types (storage) ---
+
+// Data represents the DepositAccount_Data contract storage.
+//
+//	Matches Tolk: struct DepositAccount_Data {
+//	    owner: address; proxy: address; beneficiaries: map<address, ()> }
+type Data struct {
+	Owner         *address.Address                         `tlb:"addr"`
+	Proxy         *address.Address                         `tlb:"addr"`
+	Beneficiaries *tlbe.Dict[common.AddressWrap, struct{}] `tlb:"."`
+}
+
+// --- Messages (incoming) ---
+
+// Init activates the account. Only the owner may send it.
+// Opcode: 0x6890a205
+type Init struct {
+	_              tlb.Magic  `tlb:"#6890a205" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	QueryID        uint64     `tlb:"## 64"`
+	ForwardPayload *cell.Cell `tlb:"maybe ^"`
+}
+
+// Withdraw lets a beneficiary transfer jettons from this account's wallet.
+// Opcode: 0x1936d112
+type Withdraw struct {
+	_             tlb.Magic        `tlb:"#1936d112" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	QueryID       uint64           `tlb:"## 64"`
+	WalletAddress *address.Address `tlb:"addr"`
+	Ask           *cell.Cell       `tlb:"^"` // Cell<AskToTransfer>
+}
+
+// --- Messages (outgoing) ---
+
+// Reply is sent to the initiator on successful initialization.
+// Opcode: 0xda04630c
+type Reply struct {
+	_              tlb.Magic  `tlb:"#da04630c" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	QueryID        uint64     `tlb:"## 64"`
+	ForwardPayload *cell.Cell `tlb:"maybe ^"`
+}
+
+// ForwardNotification is sent to the proxy when a message is forwarded.
+// Opcode: 0xb4fe5c0c
+type ForwardNotification struct {
+	_       tlb.Magic  `tlb:"#b4fe5c0c" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	Message *cell.Cell `tlb:"^"`                  // Cell<InMessageForward>
+}
+
+// WithdrawFailed notifies the withdraw requester that the AskToTransfer bounced.
+// Opcode: 0xa51b6cba
+type WithdrawFailed struct {
+	_             tlb.Magic        `tlb:"#a51b6cba" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	QueryID       uint64           `tlb:"## 64"`
+	WalletAddress *address.Address `tlb:"addr"`
+	Ask           *cell.Cell       `tlb:"^"` // Cell<AskToTransfer>
+}
+
+// ForwardFailed notifies the proxy when a forwarded message bounced back.
+// Opcode: 0x67dd47d3
+type ForwardFailed struct {
+	_           tlb.Magic        `tlb:"#67dd47d3" json:"-"` //nolint:revive // (opcode) should stay uninitialized
+	BouncedFrom *address.Address `tlb:"addr"`
+	BouncedBody *cell.Cell       `tlb:"^"`
+}
+
+// AskToTransfer is the standard jetton transfer request used to withdraw jettons. Reused from
+// the jetton wallet binding (opcode 0x0f8a7ea5) to avoid duplicating the type.
+type AskToTransfer = wallet.AskToTransfer
+
+var TLBs = tvm.MustNewTLBMap([]any{
+	Init{},
+	Withdraw{},
+	Reply{},
+	ForwardNotification{},
+	WithdrawFailed{},
+	ForwardFailed{},
+}).MustWithStorageType(Data{})
