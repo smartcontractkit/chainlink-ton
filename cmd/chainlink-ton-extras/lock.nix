a66eb47cc0ed32ae72fbf5706a2600fa632eec25
vicente <vicentevieytes@hotmail.com>
2026-08-18 19:13:30 -0300

    update lock.nix hash

--- cmd/chainlink-ton-extras/lock.nix
+++ cmd/chainlink-ton-extras/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton-extras = "sha256-QYDsfIy9wV6klBYsOr5b1LrZIA2qRvUVC/GjNcnBBAI=";
+  chainlink-ton-extras = "sha256-07yiojgirQTEcKIVfhugfl0ydYklhe/exVSKNqfbR34=";
 }
--- scripts/oplint/lock.nix
+++ scripts/oplint/lock.nix
@@ -1,3 +1,3 @@
 {pkgs}: {
-  oplint = "sha256-QYDsfIy9wV6klBYsOr5b1LrZIA2qRvUVC/GjNcnBBAI=";
+  oplint = "sha256-07yiojgirQTEcKIVfhugfl0ydYklhe/exVSKNqfbR34=";
 }
