613fd0cd506263f69dc17c8b515f704c25920ebb
Nicolas Mouso <nicolas.mouso@smartcontract.com>
2026-09-02 13:11:10 -0300

    nix

--- cmd/chainlink-ton/lock.nix
+++ cmd/chainlink-ton/lock.nix
@@ -1,5 +1,5 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  chainlink-ton = "sha256-MrED2i6nm0JjXesb9XofPoPn1ip17VTXggj8mxS8M24=";
+  chainlink-ton = "sha256-axWvzz20Se1F2VPD4HKCm255KpzNy8PwtkdxV+z47Kc=";
 }
