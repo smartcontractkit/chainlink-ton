6e19cec843602fc5bc12d2f64c3951f13c306ebb
vicente <vicentevieytes@hotmail.com>
2026-09-15 16:40:55 -0300

    fmt

--- contracts/tests/lib/versioning/UpgradeableSpec.ts
+++ contracts/tests/lib/versioning/UpgradeableSpec.ts
@@ -75,9 +75,7 @@ export type CurrentVersionTestConfig<TCurrentVersionContract> = {
  * Contract interface that must be implemented by upgradeable contracts for testing.
  */
 export interface UpgradeableContract
-  extends upgradeable.Interface,
-    typeAndVersion.Interface,
-    Contract {}
+  extends upgradeable.Interface, typeAndVersion.Interface, Contract {}
 
 interface PrevVersionSetup {
   version: string
