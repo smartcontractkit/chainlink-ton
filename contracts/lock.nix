693c2838bb8b9ac3e84f2ccdce0c9ed6624ca035
Nicolas Mouso <nicolas.mouso@smartcontract.com>
2026-09-17 12:12:55 -0300

    nix

--- contracts/lock.nix
+++ contracts/lock.nix
@@ -1,6 +1,6 @@
 # Notice: `pkgs.lib.fakeHash` can be used as a placeholder,
 #   but `lock-nix-tidy` will only replace actual hashes.
 {pkgs}: {
-  contracts = "sha256-z0QDQRL/XuBK7gMcZJ3Rhp/Rrsua7gKxDPbXRq5Nut8=";
+  contracts = "sha256-sG8jizoRI2CU1A+8Ixu+xHIroZd0rfw3hHm74tqhv74=";
   contracts-jetton = "sha256-EZtvTf19MjSKTWNir6pcP9XHwUIpE4ILSlhS+cQD/7w=";
 }
